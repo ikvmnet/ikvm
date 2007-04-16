16a03d5faf9b03b89e9e26c5368b72b86a042b8b
Jeroen Frijters <jeroen@frijters.net>
2007-04-16 06:06:33 +0000

    Added system property "ikvm.apartmentstate" to enable setting the COM ApartmentState for threads created in Java code.

--- classpath/java/lang/VMThread.java
+++ classpath/java/lang/VMThread.java
@@ -1,5 +1,30 @@
+/*
+  Copyright (C) 2003, 2004, 2005, 2006, 2007 Jeroen Frijters
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jeroen Frijters
+  jeroen@frijters.net
+  
+*/
 package java.lang;
 
+import cli.System.Threading.ApartmentState;
+import gnu.classpath.SystemProperties;
 import ikvm.lang.Internal;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -307,6 +332,15 @@ public final class VMThread
 	nativeThread.set_Name(thread.name);
 	nativeThread.set_IsBackground(thread.daemon);
 	nativeSetPriority(thread.priority);
+        String apartment = SystemProperties.getProperty("ikvm.apartmentstate", null);
+        if("mta".equalsIgnoreCase(apartment))
+        {
+            nativeThread.set_ApartmentState(ApartmentState.wrap(ApartmentState.MTA));
+        }
+        else if("sta".equalsIgnoreCase(apartment))
+        {
+            nativeThread.set_ApartmentState(ApartmentState.wrap(ApartmentState.STA));
+        }
 	nativeThread.Start();
 	if(!thread.daemon)
 	{
