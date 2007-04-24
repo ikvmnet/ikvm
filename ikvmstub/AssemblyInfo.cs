cdc6d17831b22b67d8b9e041bc96c6031db9672f
Jeroen Frijters <jeroen@frijters.net>
2007-04-24 08:27:34 +0000

    Updated version numbers to 0.34.0.1

--- ikvmstub/AssemblyInfo.cs
+++ ikvmstub/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.34.0.0")]
+[assembly: AssemblyVersion("0.34.0.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
