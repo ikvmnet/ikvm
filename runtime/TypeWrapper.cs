5c8d6f759b284cd2187450009fd63da6739dfff4
Jeroen Frijters <jeroen@frijters.net>
2007-05-30 11:47:24 +0000

    Back ported fix for ArrayIndexOutOfBoundsException in generic metadata reflection on dynamically compiled types.

--- runtime/TypeWrapper.cs
+++ runtime/TypeWrapper.cs
@@ -6797,7 +6797,7 @@ namespace IKVM.Internal
 				{
 					if(genericMetaData == null)
 					{
-						genericMetaData = new string[4];
+						genericMetaData = new string[classFile.Methods.Length + classFile.Fields.Length + 4];
 					}
 					genericMetaData[0] = classFile.EnclosingMethod[0];
 					genericMetaData[1] = classFile.EnclosingMethod[1];
@@ -6807,7 +6807,7 @@ namespace IKVM.Internal
 				{
 					if(genericMetaData == null)
 					{
-						genericMetaData = new string[4];
+						genericMetaData = new string[classFile.Methods.Length + classFile.Fields.Length + 4];
 					}
 					genericMetaData[3] = classFile.GenericSignature;
 				}
