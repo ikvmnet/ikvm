776352cf1d60afc3cdd3bab314bc0df4026641c3
Jeroen Frijters <jeroen@frijters.net>
2008-04-07 04:27:02 +0000

    Added Mac OS X platform detection.

--- classpath/ikvm/internal/Util.java
+++ classpath/ikvm/internal/Util.java
@@ -8,6 +8,7 @@ public final class Util
     private Util() {}
 
     public static final boolean WINDOWS = runningOnWindows();
+    public static final boolean MACOSX = runningOnMacOSX();
 
     private static boolean runningOnWindows()
     {
@@ -18,7 +19,27 @@ public final class Util
 	    || platform == cli.System.PlatformID.Win32S
 	    || platform == cli.System.PlatformID.WinCE;
     }
-	
+
+    private static boolean runningOnMacOSX()
+    {
+        cli.System.OperatingSystem os = cli.System.Environment.get_OSVersion();
+        int platform = os.get_Platform().Value;
+        if (platform == 6 /* Silverlight MacOSX PlatformID constant */)
+        {
+	    return true;
+        }
+        if (platform == cli.System.PlatformID.Unix)
+        {
+	    // we're on some sort of Unix, that probably means we're on Mono,
+	    // so we use its uname method to determine if we're on Darwin
+	    if ("Darwin".equals(MonoUtils.unameProperty("sysname")))
+	    {
+		return true;
+	    }
+        }
+        return false;
+    }
+    
     public static boolean rangeCheck(int arrayLength, int offset, int length)
     {
         return offset >= 0
