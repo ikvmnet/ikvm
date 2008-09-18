d9596fe2b4a0b75f6b302460e3b2b87c3333f821
Jeroen Frijters <jeroen@frijters.net>
2008-09-18 05:34:30 +0000

    Added tool to extract public key from ikvm-key container into tools/pubkey.txt.

---
+++ tools/pubkey.cs
@@ -0,0 +1,49 @@
+/*
+  Copyright (C) 2008 Jeroen Frijters
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jeroen Frijters
+  jeroen@frijters.net
+  
+*/
+
+using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+
+#if SIGNCODE
+    #pragma warning disable 1699
+    [assembly: AssemblyKeyName("ikvm-key")]
+#endif
+
+class PublicKey
+{
+    static void Main(string[] args)
+    {
+	StringBuilder sb = new StringBuilder();
+	foreach (byte b in Assembly.GetExecutingAssembly().GetName().GetPublicKey())
+	{
+	    sb.AppendFormat("{0:X2}", b);
+	}
+	using (StreamWriter sw = new StreamWriter("pubkey.txt"))
+	{
+	    sw.Write(sb);
+	}
+    }
+}
