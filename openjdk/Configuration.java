cb8f3bb548b9b6070dfd926a58a35ffa75e6bcb5
Jeroen Frijters <jeroen@frijters.net>
2008-11-05 07:48:43 +0000

    As usual, I forgot to update the version number here.

--- openjdk/Configuration.java
+++ openjdk/Configuration.java
@@ -28,5 +28,5 @@ package gnu.classpath;
  */
 public interface Configuration
 {
-    String default_awt_peer_toolkit = "ikvm.awt.NetToolkit, IKVM.AWT.WinForms, Version=0.38.0.0, Culture=neutral, PublicKeyToken=13235d27fcbfff58";
+    String default_awt_peer_toolkit = "ikvm.awt.NetToolkit, IKVM.AWT.WinForms, Version=0.38.0.1, Culture=neutral, PublicKeyToken=13235d27fcbfff58";
 }
