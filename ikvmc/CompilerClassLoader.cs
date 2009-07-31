0488bd2850a1b7d0ac843c2e315cb3489b27f857
Jeroen Frijters <jeroen@frijters.net>
2009-07-31 06:34:55 +0000

    Fixed regression introduced in 0.40 that caused ikvmc -classloader:<class> option to fail if <class> wasn't public.

--- ikvmc/CompilerClassLoader.cs
+++ ikvmc/CompilerClassLoader.cs
@@ -2928,7 +2928,7 @@ namespace IKVM.Internal
 					Console.Error.WriteLine("Error: custom assembly class loader class not found");
 					return 1;
 				}
-				if(!wrapper.IsPublic && !wrapper.TypeAsBaseType.Assembly.Equals(assemblyBuilder))
+				if(!wrapper.IsPublic && !ReflectUtil.IsFromAssembly(wrapper.TypeAsBaseType, assemblyBuilder))
 				{
 					Console.Error.WriteLine("Error: custom assembly class loader class is not accessible");
 					return 1;
