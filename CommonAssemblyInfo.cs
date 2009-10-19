ef0af252c79f640128763992050762d71f19167a
Jeroen Frijters <jeroen@frijters.net>
2009-10-19 04:50:41 +0000

    New release candidate version.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (C) 2002-2009 Jeroen Frijters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.42.0.0")]
+[assembly: AssemblyVersion("0.42.0.1")]
 
 #if SIGNCODE
 	#pragma warning disable 1699
