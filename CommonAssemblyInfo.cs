1cfaefdd5ecd9ddb563ef7d4e7c67b47a4b79f08
Jeroen Frijters <jeroen@frijters.net>
2009-11-30 05:45:00 +0000

    New rc version.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (C) 2002-2009 Jeroen Frijters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.42.0.2")]
+[assembly: AssemblyVersion("0.42.0.3")]
 
 #if SIGNCODE
 	#pragma warning disable 1699
