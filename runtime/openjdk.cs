e221cf948473dc6d6874b1bd58cf9e143a6e0f33
Small SQL <smallsql@sourceforge.net>
2010-01-30 15:54:38 +0000

    implements the signal handler for windows

--- runtime/openjdk.cs
+++ runtime/openjdk.cs
@@ -5428,23 +5428,118 @@ namespace IKVM.NativeCode.sun.misc
 		}
 	}
 
-	static class Signal
-	{
-		public static int findSignal(string sigName)
-		{
-			return 0;
-		}
+    static class Signal
+    {
+        /* derived from version 6.0 VC98/include/signal.h */
+        private const int SIGINT = 2;       /* interrupt */
+        private const int SIGILL = 4;       /* illegal instruction - invalid function image */
+        private const int SIGFPE = 8;       /* floating point exception */
+        private const int SIGSEGV = 11;     /* segment violation */
+        private const int SIGTERM = 15;     /* Software termination signal from kill */
+        private const int SIGBREAK = 21;    /* Ctrl-Break sequence */
+        private const int SIGABRT = 22;     /* abnormal termination triggered by abort call */
 
-		public static long handle0(int sig, long nativeH)
-		{
-			return 0;
-		}
+        private static Dictionary<int, long> handler = new Dictionary<int, long>();
 
-		public static void raise0(int sig)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        // Delegate type to be used as the Handler Routine for SetConsoleCtrlHandler
+        private delegate Boolean ConsoleCtrlDelegate(CtrlTypes CtrlType);
+
+        // Enumerated type for the control messages sent to the handler routine
+        private enum CtrlTypes : uint
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT,
+            CTRL_CLOSE_EVENT,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern bool SetConsoleCtrlHandler(ConsoleCtrlDelegate e, bool add);
+
+        private static ConsoleCtrlDelegate defaultConsoleCtrlDelegate;
+
+        private static bool ConsoleCtrlCheck(CtrlTypes ctrlType)
+        {
+#if !FIRST_PASS
+            switch (ctrlType)
+            {
+                case CtrlTypes.CTRL_BREAK_EVENT:
+                    global::java.lang.Thread.dumpAllStacks();
+                    return true;
+
+            }
+#endif
+            return false;
+        }
+
+        public static int findSignal(string sigName)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                switch (sigName)
+                {
+                    case "ABRT": /* abnormal termination triggered by abort cl */
+                        return SIGABRT;
+                    case "FPE": /* floating point exception */
+                        return SIGFPE;
+                    case "SEGV": /* segment violation */
+                        return SIGSEGV;
+                    case "INT": /* interrupt */
+                        return SIGINT;
+                    case "TERM": /* software term signal from kill */
+                        return SIGTERM;
+                    case "BREAK": /* Ctrl-Break sequence */
+                        return SIGBREAK;
+                    case "ILL": /* illegal instruction */
+                        return SIGILL;
+                }
+            }
+            return -1;
+        }
+
+        // Register a signal handler
+        public static long handle0(int sig, long nativeH)
+        {
+            long oldHandler;
+            handler.TryGetValue(sig, out oldHandler);
+            switch (nativeH)
+            {
+                case 0: // Default Signal Handler
+                    if (defaultConsoleCtrlDelegate == null && Environment.OSVersion.Platform == PlatformID.Win32NT)
+                    {
+                        defaultConsoleCtrlDelegate = new ConsoleCtrlDelegate(ConsoleCtrlCheck);
+                        SetConsoleCtrlHandler(defaultConsoleCtrlDelegate, true);
+                    }
+                    break;
+                case 1: // Ignore Signal
+                    break;
+                case 2: // Custom Signal Handler
+                    switch(sig){
+                        case SIGBREAK:
+                        case SIGFPE:
+                            return -1;
+                    }
+                    break;
+            }
+            handler[sig] = nativeH;
+            return oldHandler;
+        }
+
+        public static void raise0(int sig)
+        {
+#if !FIRST_PASS
+            global::java.security.AccessController.doPrivileged(global::ikvm.runtime.Delegates.toPrivilegedAction(delegate
+            {
+                global::java.lang.Class clazz = typeof(global::sun.misc.Signal);
+                global::java.lang.reflect.Method dispatch = clazz.getDeclaredMethod("dispatch", global::java.lang.Integer.TYPE);
+                dispatch.setAccessible(true);
+                dispatch.invoke(null, global::java.lang.Integer.valueOf(sig));
+                return null;
+            }));
+#endif
+        }
+    }
 
 	static class NativeSignalHandler
 	{
