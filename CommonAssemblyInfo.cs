3ba1905072463d96d3d32e088a589f61bca326ce
Jeroen Frijters <jeroen@frijters.net>
2010-05-18 15:01:02 +0000

    New snapshot version.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (C) 2002-2010 Jeroen Frijters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.43.3782.0")]
+[assembly: AssemblyVersion("0.43.3790.0")]
 
 #if SIGNCODE
 	#pragma warning disable 1699
