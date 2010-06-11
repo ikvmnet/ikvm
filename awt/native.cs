dadde151abb520975665dcc3db825be24bcf2b7e
Jeroen Frijters <jeroen@frijters.net>
2010-06-11 04:54:52 +0000

    Cleaned up interop structs.

--- awt/native.cs
+++ awt/native.cs
@@ -53,15 +53,17 @@ namespace IKVM.NativeCode.sun.awt.shell
 		private const uint SHGFI_ICON = 0x100;
 		private const uint SHGFI_TYPENAME = 0x400;
 		private const uint SHGFI_ATTRIBUTES = 0x800;
+
+		[StructLayout(LayoutKind.Sequential)]
 		private struct SHFILEINFO
 		{
-			public IntPtr hIcon;
-			public IntPtr iIcon;
-			public uint dwAttributes;
+			internal IntPtr hIcon;
+			internal IntPtr iIcon;
+			internal uint dwAttributes;
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-			public string szDisplayName;
+			internal string szDisplayName;
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
-			public string szTypeName;
+			internal string szTypeName;
 		};
 
 		[DllImport("gdi32.dll")]
@@ -78,38 +80,34 @@ namespace IKVM.NativeCode.sun.awt.shell
 		public static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
 
 		[StructLayout(LayoutKind.Sequential)]
-		struct ICONINFO
+		private struct ICONINFO
 		{
-			public bool fIcon;         // Specifies whether this structure defines an icon or a cursor. A value of TRUE specifies 
-			// an icon; FALSE specifies a cursor. 
-			public Int32 xHotspot;     // Specifies the x-coordinate of a cursor's hot spot. If this structure defines an icon, the hot 
-			// spot is always in the center of the icon, and this member is ignored.
-			public Int32 yHotspot;     // Specifies the y-coordinate of the cursor's hot spot. If this structure defines an icon, the hot 
-			// spot is always in the center of the icon, and this member is ignored. 
-			public IntPtr hbmMask;     // (HBITMAP) Specifies the icon bitmask bitmap. If this structure defines a black and white icon, 
-			// this bitmask is formatted so that the upper half is the icon AND bitmask and the lower half is 
-			// the icon XOR bitmask. Under this condition, the height should be an even multiple of two. If 
-			// this structure defines a color icon, this mask only defines the AND bitmask of the icon. 
-			public IntPtr hbmColor;    // (HBITMAP) Handle to the icon color bitmap. This member can be optional if this 
-			// structure defines a black and white icon. The AND bitmask of hbmMask is applied with the SRCAND 
-			// flag to the destination; subsequently, the color bitmap is applied (using XOR) to the 
-			// destination by using the SRCINVERT flag. 
+			internal bool fIcon;
+			internal int xHotspot;
+			internal int yHotspot;
+			internal IntPtr hbmMask;
+			internal IntPtr hbmColor;
 		}
 
 		[DllImport("user32.dll")]
 		static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
 
 		[StructLayout(LayoutKind.Sequential)]
-		public struct BITMAPINFO
+		private struct BITMAPINFO
 		{
-			public uint biSize;
-			public int biWidth, biHeight;
-			public short biPlanes, biBitCount;
-			public uint biCompression, biSizeImage;
-			public int biXPelsPerMeter, biYPelsPerMeter;
-			public uint biClrUsed, biClrImportant;
+			internal uint biSize;
+			internal int biWidth;
+			internal int biHeight;
+			internal ushort biPlanes;
+			internal ushort biBitCount;
+			internal uint biCompression;
+			internal uint biSizeImage;
+			internal int biXPelsPerMeter;
+			internal int biYPelsPerMeter;
+			internal uint biClrUsed;
+			internal uint biClrImportant;
 			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
-			public uint[] cols;
+			internal uint[] cols;
 		}
 
 		[DllImport("shell32.dll")]
