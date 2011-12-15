ec511e1bacb9596017cb84f02da667a4caf5423a
Jeroen Frijters <jeroen@frijters.net>
2011-12-15 14:51:15 +0000

    Corrected comment.

--- runtime/MemberWrapper.cs
+++ runtime/MemberWrapper.cs
@@ -1103,7 +1103,7 @@ namespace IKVM.Internal
 			if(DeclaringType.IsNonPrimitiveValueType)
 			{
 				// callvirt isn't allowed on a value type
-				// TODO we need to check for a null reference
+				// (we don't need to check for a null reference, because we're always dealing with an unboxed value)
 				CallImpl(ilgen);
 			}
 			else
