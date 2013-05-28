bc8a6bae0cc4c93a5142463962de6639dc53ba7c
Jeroen Frijters <jeroen@frijters.net>
2013-05-28 11:02:14 +0000

    Bug fix. An ITEMIDLIST should be freed using CoTaskMemFree.

--- awt/native.cs
+++ awt/native.cs
@@ -566,7 +566,7 @@ namespace IKVM.NativeCode.sun.awt.shell
             {
                 return;
             }
-            Marshal.Release(pIDL);
+            Marshal.FreeCoTaskMem(pIDL);
         }
 
 		/// <summary>
