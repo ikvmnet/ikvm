948b26518f95aae6b57cb665cde05e0c1ba0bac6
Jeroen Frijters <jeroen@frijters.net>
2014-05-14 07:08:03 +0000

    Fixed regression in previous commit.

--- runtime/compiler.cs
+++ runtime/compiler.cs
@@ -2897,7 +2897,7 @@ sealed class Compiler
 		if (tw.IsUnloadable)
 		{
 			Profiler.Count("EmitDynamicClassLiteral");
-			EmitDynamicClassLiteral(tw);
+			context.EmitDynamicClassLiteral(ilgen, tw);
 		}
 		else
 		{
