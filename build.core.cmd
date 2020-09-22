32fab9b88a049002cd1d7d2eab37efe1bbc19adb
Igor Kolesnik <igork@windward.net>
2020-09-22 05:10:18 -0700

    Uncomment what can be built

--- build.core.cmd
+++ build.core.cmd
@@ -1,17 +1,17 @@
 @echo off
 
-rem dotnet build -f netcoreapp3.1 tools\updbaseaddresses\updbaseaddresses.csproj
-rem dotnet build -f netcoreapp3.1 tools\depcheck\depcheck.csproj
-rem dotnet build -f netcoreapp3.1 tools\pubkey\pubkey.csproj
-rem dotnet build -f netcoreapp3.1 tools\SourceLicenseAnalyzer\SourceLicenseAnalyzer.csproj
-rem dotnet build -f netcoreapp3.1 reflect\IKVM.Reflection.csproj
-rem dotnet build -f netcoreapp3.1 ikvmstub\ikvmstub.csproj
-rem dotnet build -f netcoreapp3.1 runtime\DummyLibrary\DummyLibrary.csproj
-rem dotnet build -f netcoreapp3.1 runtime\IKVM.Runtime.FirstPass\IKVM.Runtime.FirstPass.csproj
-rem dotnet build -f netcoreapp3.1 awt\IKVM.AWT.WinForms.FirstPass\IKVM.AWT.WinForms.FirstPass.csproj
-rem dotnet build -f netcoreapp3.1 ikvmc\ikvmc.csproj
-rem dotnet build -f netcoreapp3.1 openjdk\openjdk.csproj
-rem dotnet build -f netcoreapp3.1 runtime\IKVM.Runtime.JNI\IKVM.Runtime.JNI.csproj
+dotnet build -f netcoreapp3.1 tools\updbaseaddresses\updbaseaddresses.csproj
+dotnet build -f netcoreapp3.1 tools\depcheck\depcheck.csproj
+dotnet build -f netcoreapp3.1 tools\pubkey\pubkey.csproj
+dotnet build -f netcoreapp3.1 tools\SourceLicenseAnalyzer\SourceLicenseAnalyzer.csproj
+dotnet build -f netcoreapp3.1 reflect\IKVM.Reflection.csproj
+dotnet build -f netcoreapp3.1 ikvmstub\ikvmstub.csproj
+dotnet build -f netcoreapp3.1 runtime\DummyLibrary\DummyLibrary.csproj
+dotnet build -f netcoreapp3.1 runtime\IKVM.Runtime.FirstPass\IKVM.Runtime.FirstPass.csproj
+dotnet build -f netcoreapp3.1 awt\IKVM.AWT.WinForms.FirstPass\IKVM.AWT.WinForms.FirstPass.csproj
+dotnet build -f netcoreapp3.1 ikvmc\ikvmc.csproj
+dotnet build -f netcoreapp3.1 openjdk\openjdk.csproj
+dotnet build -f netcoreapp3.1 runtime\IKVM.Runtime.JNI\IKVM.Runtime.JNI.csproj
 dotnet build -f netcoreapp3.1 runtime\IKVM.Runtime\IKVM.Runtime.csproj
 rem dotnet build -f netcoreapp3.1 openjdk\openjdk.tools.csproj
 rem dotnet build -f netcoreapp3.1 awt\IKVM.AWT.WinForms\IKVM.AWT.WinForms.csproj
