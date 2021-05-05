2ac44536cb9774e3c5e5f7e56474f9dc34283861
Igor Kolesnik <igork@windward.net>
2021-05-05 19:11:47 +0300

    Minor refactoring

--- build.core.cmd
+++ build.core.cmd
@@ -1,29 +1,32 @@
 @echo off
 
 set CONFIGURATION=Release
+set TARGETFRAMEWORK=netcoreapp3.1
 
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 tools\updbaseaddresses\updbaseaddresses.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 tools\depcheck\depcheck.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 tools\pubkey\pubkey.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 tools\SourceLicenseAnalyzer\SourceLicenseAnalyzer.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 reflect\IKVM.Reflection.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 ikvmstub\ikvmstub.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 runtime\DummyLibrary\DummyLibrary.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 runtime\IKVM.Runtime.FirstPass\IKVM.Runtime.FirstPass.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 awt\IKVM.AWT.WinForms.FirstPass\IKVM.AWT.WinForms.FirstPass.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 ikvmc\ikvmc.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 openjdk\openjdk.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 runtime\IKVM.Runtime.JNI\IKVM.Runtime.JNI.csproj
+set BUILDFLAGS=--nologo --no-dependencies -c %CONFIGURATION% -f %TARGETFRAMEWORK%
+
+dotnet build %BUILDFLAGS% tools\updbaseaddresses\updbaseaddresses.csproj
+dotnet build %BUILDFLAGS% tools\depcheck\depcheck.csproj
+dotnet build %BUILDFLAGS% tools\pubkey\pubkey.csproj
+dotnet build %BUILDFLAGS% tools\SourceLicenseAnalyzer\SourceLicenseAnalyzer.csproj
+dotnet build %BUILDFLAGS% reflect\IKVM.Reflection.csproj
+dotnet build %BUILDFLAGS% ikvmstub\ikvmstub.csproj
+dotnet build %BUILDFLAGS% runtime\DummyLibrary\DummyLibrary.csproj
+dotnet build %BUILDFLAGS% runtime\IKVM.Runtime.FirstPass\IKVM.Runtime.FirstPass.csproj
+dotnet build %BUILDFLAGS% awt\IKVM.AWT.WinForms.FirstPass\IKVM.AWT.WinForms.FirstPass.csproj
+dotnet build %BUILDFLAGS% ikvmc\ikvmc.csproj
+dotnet build %BUILDFLAGS% openjdk\openjdk.csproj
+dotnet build %BUILDFLAGS% runtime\IKVM.Runtime.JNI\IKVM.Runtime.JNI.csproj
 rem Remove first-pass runtime binaries.
 rem Build fails if there is IKVM.Runtime.dll
-del /F /Q bin\%CONFIGURATION%\netcoreapp3.1\IKVM.Runtime.dll bin\%CONFIGURATION%\netcoreapp3.1\IKVM.Runtime.deps.json bin\%CONFIGURATION%\netcoreapp3.1\IKVM.Runtime.xml bin\%CONFIGURATION%\netcoreapp3.1\IKVM.Runtime.pdb
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 runtime\IKVM.Runtime\IKVM.Runtime.csproj
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 openjdk\openjdk.tools.csproj
+del /F /Q bin\%CONFIGURATION%\%TARGETFRAMEWORK%\IKVM.Runtime.dll bin\%CONFIGURATION%\%TARGETFRAMEWORK%\IKVM.Runtime.deps.json bin\%CONFIGURATION%\%TARGETFRAMEWORK%\IKVM.Runtime.xml bin\%CONFIGURATION%\%TARGETFRAMEWORK%\IKVM.Runtime.pdb
+dotnet build %BUILDFLAGS% runtime\IKVM.Runtime\IKVM.Runtime.csproj
+dotnet build %BUILDFLAGS% openjdk\openjdk.tools.csproj
 rem Remove first-pass awt binaries.
 rem Build fails if there is IKVM.AWT.WinForms.dll
-del /F /Q bin\%CONFIGURATION%\netcoreapp3.1\IKVM.AWT.WinForms.dll bin\%CONFIGURATION%\netcoreapp3.1\IKVM.AWT.WinForms.deps.json bin\%CONFIGURATION%\netcoreapp3.1\IKVM.AWT.WinForms.xml bin\%CONFIGURATION%\netcoreapp3.1\IKVM.AWT.WinForms.pdb
-dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 awt\IKVM.AWT.WinForms\IKVM.AWT.WinForms.csproj
+del /F /Q bin\%CONFIGURATION%\%TARGETFRAMEWORK%\IKVM.AWT.WinForms.*
+dotnet build %BUILDFLAGS% awt\IKVM.AWT.WinForms\IKVM.AWT.WinForms.csproj
 
-rem dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 tools\implib\implib.csproj
-rem dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 ikvm\ikvm.csproj
-rem dotnet build --no-dependencies -c %CONFIGURATION% -f netcoreapp3.1 jvm\jvm.csproj
+rem dotnet build %BUILDFLAGS% tools\implib\implib.csproj
+rem dotnet build %BUILDFLAGS% ikvm\ikvm.csproj
+rem dotnet build %BUILDFLAGS% jvm\jvm.csproj
