bd562679174dec4b7aabf85b094165cefd35c6ae
Jerome Haltom <jhaltom@alethic.solutions>
2022-05-15 19:02:54 -0500

    Added two simple test cases.

---
+++ rebuild/IKVM.Tests/IKVMTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using System;
+
+namespace IKVM.Tests
+{
+
+    [TestClass]
+    public class IKVMTest
+    {
+
+        [TestMethod]
+        public void Can_resolve_object_type()
+        {
+            var o = (Type)java.lang.Class.forName("java.lang.Object");
+            o.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void Can_resolve_string_type()
+        {
+            var o = (Type)java.lang.Class.forName("java.lang.String");
+            o.Should().NotBeNull();
+        }
+
+    }
+
+}
