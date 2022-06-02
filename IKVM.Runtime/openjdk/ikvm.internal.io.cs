f1facd31ec673d2ad60e89aea4b604714aa8e79a
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-02 10:25:35 -0500

    So it looks like the FileStreamAclExtensions code is broken in NET Core < 6. Seems they're trying to fix it for NET 7. However, eh. So we revert back to the logic originally in NetFileSystemProvider.

--- IKVM.Runtime/openjdk/ikvm.internal.io.cs
+++ IKVM.Runtime/openjdk/ikvm.internal.io.cs
@@ -9,7 +9,15 @@ static class Java_ikvm_internal_io_FileStreamExtensions
 #if NET461
         return new FileStream(path, mode, rights, share, bufferSize, options);
 #else
-        return FileSystemAclExtensions.Create(new FileInfo(path), mode, rights, share, bufferSize, options, new FileSecurity());
+            var access = 0;
+            if ((rights & FileSystemRights.Read) != 0)
+                access |= (int)FileAccess.Read;
+            if ((rights & (FileSystemRights.Write | FileSystemRights.AppendData)) != 0)
+                access |= (int)FileAccess.Write;
+            if (access == 0)
+                access = (int)FileAccess.ReadWrite;
+
+        return new FileStream(path, mode, (FileAccess)access, share, bufferSize, options);
 #endif
     }
 
---
+++ IKVM.Tests/java/nio/channels/FileChannelTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.java.nio.channels
+{
+
+    [TestClass]
+    public class FileChannelTests
+    {
+
+        [TestMethod]
+        public void Can_open_filechannel_write()
+        {
+            var f = new global::java.io.File(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+            f.createNewFile();
+            var o = new global::java.util.HashSet();
+            o.add(global::java.nio.file.StandardOpenOption.WRITE);
+            o.add(global::java.nio.file.StandardOpenOption.TRUNCATE_EXISTING);
+            var c = global::java.nio.channels.FileChannel.open(f.toPath(), o);
+            c.close();
+        }
+
+    }
+
+}
