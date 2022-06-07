5239d2fd82e29ec23a020b13297f26a0faa63dbc
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-07 16:38:39 -0500

    Moved some test utilties to Tests.Utils. Added an ikvmc.Tests project to give a simple run of ikvmc.

--- src/IKVM.Tests/JNI/JniTests.cs
+++ src/IKVM.Tests/JNI/JniTests.cs
@@ -3,6 +3,8 @@ using System.IO;
 
 using FluentAssertions;
 
+using IKVM.Tests.Util;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IKVM.Tests.JNI
@@ -20,7 +22,7 @@ namespace IKVM.Tests.JNI
             var f = new InMemoryCodeUnit("ikvm.tests.jni.JniTests", s);
             var c = new InMemoryCompiler(new[] { f });
             c.Compile();
-            var z = c.getCompiledClass("ikvm.tests.jni.JniTests");
+            var z = c.GetClass("ikvm.tests.jni.JniTests");
             if (z == null)
                 throw new Exception();
 
