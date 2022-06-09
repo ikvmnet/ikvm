3a3e8fbd306432a6cb63115a3f5ae4164c89ebf7
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-09 14:43:48 -0500

    Add Maven project.

---
+++ src/IKVM.Sdk.Maven/Tasks/MavenTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Build.Utilities;
+
+namespace IKVM.Sdk.Maven.Tasks
+{
+
+    public class MavenTask : Task
+    {
+
+        public override bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
