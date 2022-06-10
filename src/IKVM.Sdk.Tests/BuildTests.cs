b24bbe3252219b3c0ea84b63408f5d8f22943773
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-10 00:34:39 -0500

    Add test project for SDK. Firing up a test csproj in memory.

---
+++ src/IKVM.Sdk.Tests/BuildTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+using Buildalyzer;
+
+using FluentAssertions;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Sdk.Tests
+{
+
+    [TestClass]
+    public class BuildTests
+    {
+
+        /// <summary>
+        /// Forwards MSBuild events to the test context.
+        /// </summary>
+        class TargetLogger : Logger
+        {
+
+            readonly TestContext context;
+
+            /// <summary>
+            /// Initializes a new instance.
+            /// </summary>
+            /// <param name="context"></param>
+            /// <exception cref="ArgumentNullException"></exception>
+            public TargetLogger(TestContext context)
+            {
+                this.context = context ?? throw new ArgumentNullException(nameof(context));
+            }
+
+            public override void Initialize(IEventSource eventSource)
+            {
+                eventSource.AnyEventRaised += (sender, evt) => context.WriteLine($"{evt.Message}");
+            }
+
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Can_build_test_project()
+        {
+            var nugetPackageRoot = Path.Combine(Path.GetTempPath(), "IKVM.Sdk.Tests", "nuget", "packages");
+            if (Directory.Exists(nugetPackageRoot))
+                Directory.Delete(nugetPackageRoot, true);
+            Directory.CreateDirectory(nugetPackageRoot);
+
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(Path.Combine(@"Project", "Project.csproj"));
+            analyzer.SetGlobalProperty("RestoreAdditionalProjectSources", Path.GetFullPath(@"nuget"));
+            analyzer.SetGlobalProperty("RestorePackagesPath", nugetPackageRoot + Path.DirectorySeparatorChar);
+            analyzer.AddBuildLogger(new TargetLogger(TestContext));
+            var results = analyzer.Build();
+            results.OverallSuccess.Should().Be(true);
+        }
+
+    }
+
+}
