fd44087f5471e609c9dda22504c62b8265463a34
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-12 12:31:38 -0500

    Remove net472 target from Ikvm.SDK. 461 is sufficient. Test project compiles towards net472. Need more of these. Fix ref.

--- src/IKVM.Sdk.Tests/Project/Exe/Program.cs
+++ src/IKVM.Sdk.Tests/Project/Exe/Program.cs
@@ -13,7 +13,7 @@ namespace IKVM.Sdk.Tests.Project.Exe
         public static void Main(string[] args)
         {
             Console.WriteLine(Helloworld.SayHello(args[0]));
-            Console.WriteLine(Helloworld.SayHello21(args[0]));
+            Console.WriteLine(Helloworld.SayHello2(args[0]));
         }
 
     }
