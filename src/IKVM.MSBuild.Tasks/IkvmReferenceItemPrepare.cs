b6de8429ca6ea9d39494fdd1039e108545dfccdf
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-30 09:57:10 -0500

    Don't sort references. Technically the sort order is important, as different assemblies first could cause a different class to be resolved first.

--- src/IKVM.MSBuild.Tasks/IkvmReferenceItemPrepare.cs
+++ src/IKVM.MSBuild.Tasks/IkvmReferenceItemPrepare.cs
@@ -360,7 +360,7 @@ namespace IKVM.MSBuild.Tasks
                 references.Add(GetReferenceLine(item, reference));
 
             // sort and write the reference lines
-            foreach (var r in references.OrderBy(i => i))
+            foreach (var r in references)
                 manifest.WriteLine(r);
 
             // hash the resulting manifest and set the identity
