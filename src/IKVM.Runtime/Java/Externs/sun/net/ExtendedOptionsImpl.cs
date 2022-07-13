cbac174a4999de5c92c81a91daaba6d2c60727af
Jerome Haltom <jhaltom@alethic.solutions>
2022-07-13 13:53:24 -0500

    Add ExtendedOptionsImple.

---
+++ src/IKVM.Runtime/Java/Externs/sun/net/ExtendedOptionsImpl.cs
@@ -0,0 +1,29 @@
+﻿namespace IKVM.Runtime.Java.Externs.sun.net
+{
+
+    static class ExtendedOptionsImpl
+    {
+
+        public static void init()
+        {
+
+        }
+
+        public static bool flowSupported()
+        {
+            return false;
+        }
+
+        public static void getFlowOption(object this_, object f_)
+        {
+            throw new global::java.lang.UnsupportedOperationException();
+        }
+
+        public static void setFlowOption(object this_, object f_)
+        {
+            throw new global::java.lang.UnsupportedOperationException();
+        }
+
+    }
+
+}
