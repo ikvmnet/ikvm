1748fc035c025f1aadbb716c7fff51fcdb6df595
Jerome Haltom <jhaltom@alethic.solutions>
2022-07-26 13:11:43 -0500

    Add test stuff back.

---
+++ src/IKVM.JavaTest.AgentServer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace IKVM.JavaTest.AgentServer
+{
+
+    public static class Program
+    {
+         
+        public static void Main(string[] args)
+        {
+            new com.sun.javatest.regtest.agent.AgentServer(args).run();
+        }
+
+    }
+
+}
---
+++ src/IKVM.JavaTest.TestAdapter/DelegateErrorHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using com.sun.javatest;
+
+namespace IKVM.JavaTest
+{
+
+    /// <summary>
+    /// Implementation of <see cref="TestFinder.ErrorHandler"/> that invokes a delegate.
+    /// </summary>
+    class DelegateErrorHandler : TestFinder.ErrorHandler
+    {
+
+        readonly Action<string> onError;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="onError"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public DelegateErrorHandler(Action<string> onError)
+        {
+            this.onError = onError ?? throw new ArgumentNullException(nameof(onError));
+        }
+
+        public void error(string str)
+        {
+            onError(str);
+        }
+
+    }
+
+}
