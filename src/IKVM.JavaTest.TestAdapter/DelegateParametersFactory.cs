1c780b0374d8ad6c8606241253e29b73fc38dc63
Jerome Haltom <jhaltom@alethic.solutions>
2022-08-31 09:14:21 -0500

    Revert "Remove JavaTest projects. Splitting out new SDK features."
    This reverts commit 5d7ef980dd7374b019a2c770701d97ca9d2b1fa1.

---
+++ src/IKVM.JavaTest.TestAdapter/DelegateParametersFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using com.sun.javatest.regtest.config;
+
+namespace IKVM.JavaTest
+{
+
+    /// <summary>
+    /// Implementation of <see cref="RegressionTestSuite.ParametersFactory"/> that invokes a delegate.
+    /// </summary>
+    class ParametersFactory : RegressionTestSuite.ParametersFactory
+    {
+
+        readonly Func<RegressionTestSuite, RegressionParameters> onCreate;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="onCreate"></param>
+        public ParametersFactory(Func<RegressionTestSuite, RegressionParameters> onCreate)
+        {
+            this.onCreate = onCreate;
+        }
+
+        public RegressionParameters create(RegressionTestSuite rts)
+        {
+            return onCreate(rts);
+        }
+
+    }
+
+}
