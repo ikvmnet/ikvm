b5145731e3ee3f2e6757181cea100591a4849864
Jerome Haltom <jhaltom@alethic.solutions>
2022-09-18 20:27:10 -0500

    Fix reference.

--- src/ikvm/Program.cs
+++ src/ikvm/Program.cs
@@ -1,13 +1,10 @@
-namespace ikvm
+namespace ikvm.tools.ikvm
 {
 
     public static class Program
     {
 
-        public static int Main(string[] args)
-        {
-            return java.Program.Main(args);
-        }
+        public static int Main(string[] args) => global::ikvm.tools.java.Program.Main(args);
 
     }
 
