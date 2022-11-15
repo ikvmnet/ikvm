891bf54a0528ca2d39b2d5bf5709d31f7fd413f4
Jerome Haltom <jhaltom@alethic.solutions>
2022-11-14 23:56:04 -0600

    Add PlatformTarget to SDK output. Clean up some Native lookups.

--- src/IKVM.Tests/Native.cs
+++ src/IKVM.Tests/Native.cs
@@ -30,11 +30,13 @@ namespace IKVM.Tests
         /// Gets the RID architecture.
         /// </summary>
         /// <returns></returns>
-        static string GetRuntimeIdentifierArch() => IntPtr.Size switch
+        static string GetRuntimeIdentifierArch() => RuntimeInformation.ProcessArchitecture switch
         {
-            4 => "x86",
-            8 => "x64",
-            _ => throw new NotSupportedException(),
+            Architecture.X86 => "x86",
+            Architecture.X64 => "x64",
+            Architecture.Arm => "arm",
+            Architecture.Arm64 => "arm64",
+            _ => null,
         };
 
         /// <summary>
@@ -44,16 +46,33 @@ namespace IKVM.Tests
         static IEnumerable<string> GetRuntimeIdentifiers()
         {
             var arch = GetRuntimeIdentifierArch();
+            if (arch == null)
+                yield break;
 
-#if NETFRAMEWORK
-            yield return $"win-{arch}";
-#else
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var v = Environment.OSVersion.Version;
+
+                // Windows 10
+                if (v.Major > 10 || (v.Major == 10 && v.Minor >= 0))
+                    yield return $"win10-{arch}";
+
+                // Windows 8.1
+                if (v.Major > 6 || (v.Major == 6 && v.Minor >= 3))
+                    yield return $"win81-{arch}";
+
+                // Windows 7
+                if (v.Major > 6 || (v.Major == 6 && v.Minor >= 1))
+                    yield return $"win7-{arch}";
+
+                // fallback
                 yield return $"win-{arch}";
+            }
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
                 yield return $"linux-{arch}";
-#endif
+            }
         }
 
         /// <summary>
@@ -63,15 +82,11 @@ namespace IKVM.Tests
         /// <returns></returns>
         static string GetLibraryFileName(string name)
         {
-#if NET461
-            return $"{name}.dll";
-#else
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return $"{name}.dll";
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 return $"lib{name}.so";
-#endif
 
             throw new NotSupportedException();
         }
