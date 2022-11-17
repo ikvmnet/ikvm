d6b249802a30f8b7c43098c37660524a83be0dd1
Jerome Haltom <jhaltom@alethic.solutions>
2022-11-17 08:21:47 -0600

    Add missing native implementation for sun.java2d.Disposer.initIDs.
    fixes #210 maybe

---
+++ src/IKVM.Runtime/Java/Externs/sun/java2d/Disposer.cs
@@ -0,0 +1,20 @@
+﻿namespace IKVM.Runtime.Java.Externs.sun.java2d
+{
+
+    /// <summary>
+    /// Implements the external methods for <see cref="global::sun.java2d.Disposer"/>.
+    /// </summary>
+    static class Disposer
+    {
+
+        /// <summary>
+        /// Implements the native method for 'init'.
+        /// </summary>
+        public static void initIDs()
+        {
+
+        }
+
+    }
+
+}
