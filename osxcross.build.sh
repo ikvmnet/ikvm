5b8d1f075f0510fbc15de0fc4627e1d65e839c57
Jerome Haltom <jhaltom@alethic.solutions>
2022-11-17 22:10:12 -0600

    Don't build GCC.

--- osxcross.build.sh
+++ osxcross.build.sh
@@ -2,4 +2,3 @@
 curl -L https://github.com/joseluisq/macosx-sdks/releases/download/11.1/MacOSX11.1.sdk.tar.xz -o osxcross/tarballs/MacOSX11.1.sdk.tar.xz
 cd osxcross
 UNATTENDED=1 ./build.sh
-UNATTENDED=1 ./build_gcc.sh
