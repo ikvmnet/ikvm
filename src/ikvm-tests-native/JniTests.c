5d5f490054af60e1699eeb3eef9411aa03abc33f
Jerome Haltom <jhaltom@alethic.solutions>
2022-11-22 10:48:33 -0600

    Fix build.

--- src/ikvm-tests-native/JniTests.c
+++ src/ikvm-tests-native/JniTests.c
@@ -4,9 +4,9 @@
 JNIEXPORT jstring JNICALL Java_ikvm_tests_jni_JniTests_echoImpl(JNIEnv* env, jclass cls, jstring value)
 {
 	jstring ret;
-	char* tmp;
+	const char* tmp;
 
-	tmp = (*env)->GetStringUTFChars(env, value, NULL);
+	tmp = (*env)->GetStringUTFChars(env, value, 0);
 	ret = (*env)->NewStringUTF(env, tmp);
 
 	return ret;
