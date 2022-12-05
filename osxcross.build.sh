a65ee01e155c14aae5eb35952d82192c41ccf758
Jerome Haltom <jhaltom@alethic.solutions>
2022-12-05 09:16:57 -0600

    Remove osxcross.

--- osxcross.build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -L https://github.com/joseluisq/macosx-sdks/releases/download/11.1/MacOSX11.1.sdk.tar.xz -o osxcross/tarballs/MacOSX11.1.sdk.tar.xz
-cd osxcross
-UNATTENDED=1 ./build.sh
