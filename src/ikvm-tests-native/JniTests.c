078b3f687de73fb5b0fc2759fad14fb66af8aa26
Jerome Haltom <jhaltom@alethic.solutions>
2022-12-29 14:31:51 -0600

    C standard for pointer.

--- src/ikvm-tests-native/JniTests.c
+++ src/ikvm-tests-native/JniTests.c
@@ -274,12 +274,12 @@ JNIEXPORT void JNICALL Java_ikvm_tests_jni_JniTests_newObjectATestWithArg(JNIEnv
     }
 }
 
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)
 {
 	return JNI_VERSION_1_8;
 }
 
-JNIEXPORT void JNICALL JNI_OnUnload(JavaVM* vm, void* reserved)
+JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved)
 {
 
 }
