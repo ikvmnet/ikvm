6b8349e1937eff3eee9dc980c5ea847de93cd9b0
Jerome Haltom <jhaltom@alethic.solutions>
2023-01-02 11:11:19 -0600

    Clean up. Use appropriate SHA1 implementation.

--- src/IKVM.Runtime/StubGen/SerialVersionUID.cs
+++ src/IKVM.Runtime/StubGen/SerialVersionUID.cs
@@ -21,163 +21,132 @@
   jeroen@frijters.net
   
 */
-using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 
 using IKVM.Attributes;
 using IKVM.Internal;
-#if EXPORTER
-using Type = IKVM.Reflection.Type;
-#endif
 
 namespace IKVM.StubGen
 {
+
     static class SerialVersionUID
-	{
-		private readonly static System.Security.Cryptography.SHA1Managed sha1 = new System.Security.Cryptography.SHA1Managed();
+    {
 
-		internal static long Compute(TypeWrapper tw)
-		{
-			MemoryStream mem = new MemoryStream();
-			BigEndianStream bes = new BigEndianStream(mem);
-			WriteClassName(bes, tw);
-			WriteModifiers(bes, tw);
-			WriteInterfaces(bes, tw);
-			WriteFields(bes, tw);
-			WriteStaticInitializer(bes, tw);
-			WriteConstructors(bes, tw);
-			WriteMethods(bes, tw);
-			byte[] buf = sha1.ComputeHash(mem.ToArray());
-			long hash = 0;
-			for (int i = 7; i >= 0; i--)
-			{
-				hash <<= 8;
-				hash |= buf[i];
-			}
-			return hash;
-		}
+        readonly static SHA1 sha1 = SHA1.Create();
 
-		private static void WriteClassName(BigEndianStream bes, TypeWrapper tw)
-		{
-			bes.WriteUtf8(tw.Name);
-		}
+        internal static long Compute(TypeWrapper tw)
+        {
+            var mem = new MemoryStream();
+            var bes = new BigEndianStream(mem);
+            WriteClassName(bes, tw);
+            WriteModifiers(bes, tw);
+            WriteInterfaces(bes, tw);
+            WriteFields(bes, tw);
+            WriteStaticInitializer(bes, tw);
+            WriteConstructors(bes, tw);
+            WriteMethods(bes, tw);
+            mem.Position = 0;
+            var buf = sha1.ComputeHash(mem);
+            var hash = 0;
+            for (var i = 7; i >= 0; i--)
+            {
+                hash <<= 8;
+                hash |= buf[i];
+            }
 
-		private static void WriteModifiers(BigEndianStream bes, TypeWrapper tw)
-		{
-			Modifiers mods = tw.ReflectiveModifiers & (Modifiers.Public | Modifiers.Final | Modifiers.Interface | Modifiers.Abstract);
-			if ((mods & Modifiers.Interface) != 0)
-			{
-				mods &= ~Modifiers.Abstract;
-				if (HasJavaMethods(tw))
-				{
-					mods |= Modifiers.Abstract;
-				}
-			}
-			bes.WriteUInt32((uint)mods);
-		}
+            return hash;
+        }
 
-		private static bool HasJavaMethods(TypeWrapper tw)
-		{
-			foreach (MethodWrapper mw in tw.GetMethods())
-			{
-				if (!mw.IsHideFromReflection && !mw.IsClassInitializer)
-				{
-					return true;
-				}
-			}
-			return false;
-		}
+        static void WriteClassName(BigEndianStream bes, TypeWrapper tw)
+        {
+            bes.WriteUtf8(tw.Name);
+        }
 
-		private static void WriteInterfaces(BigEndianStream bes, TypeWrapper tw)
-		{
-			TypeWrapper[] interfaces = (TypeWrapper[])tw.Interfaces.Clone();
-			Array.Sort(interfaces, delegate(TypeWrapper tw1, TypeWrapper tw2) { return String.CompareOrdinal(tw1.Name, tw2.Name); });
-			foreach (TypeWrapper iface in interfaces)
-			{
-				bes.WriteUtf8(iface.Name);
-			}
-		}
+        static void WriteModifiers(BigEndianStream bes, TypeWrapper tw)
+        {
+            var mods = tw.ReflectiveModifiers & (Modifiers.Public | Modifiers.Final | Modifiers.Interface | Modifiers.Abstract);
+            if ((mods & Modifiers.Interface) != 0)
+            {
+                mods &= ~Modifiers.Abstract;
+                if (HasJavaMethods(tw))
+                    mods |= Modifiers.Abstract;
+            }
 
-		private static void WriteFields(BigEndianStream bes, TypeWrapper tw)
-		{
-			List<FieldWrapper> list = new List<FieldWrapper>();
-			foreach (FieldWrapper fw in tw.GetFields())
-			{
-				if (!fw.IsHideFromReflection)
-				{
-					list.Add(fw);
-				}
-			}
-			list.Sort(delegate(FieldWrapper fw1, FieldWrapper fw2) { return String.CompareOrdinal(fw1.Name, fw2.Name); });
-			foreach (FieldWrapper fw in list)
-			{
-				Modifiers mods = fw.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Volatile | Modifiers.Transient);
-				if (((mods & Modifiers.Private) == 0) || ((mods & (Modifiers.Static | Modifiers.Transient)) == 0))
-				{
-					bes.WriteUtf8(fw.Name);
-					bes.WriteUInt32((uint)mods);
-					bes.WriteUtf8(fw.Signature.Replace('.', '/'));
-				}
-			}
-		}
+            bes.WriteUInt32((uint)mods);
+        }
 
-		private static void WriteStaticInitializer(BigEndianStream bes, TypeWrapper tw)
-		{
-			Type type = tw.TypeAsTBD;
-			if (!type.IsArray && type.TypeInitializer != null)
-			{
-				if (!AttributeHelper.IsHideFromJava(type.TypeInitializer))
-				{
-					bes.WriteUtf8("<clinit>");
-					bes.WriteUInt32((uint)Modifiers.Static);
-					bes.WriteUtf8("()V");
-				}
-			}
-		}
+        static bool HasJavaMethods(TypeWrapper tw)
+        {
+            return tw.GetMethods().Any(i => !i.IsHideFromReflection && !i.IsClassInitializer);
+        }
 
-		private static void WriteConstructors(BigEndianStream bes, TypeWrapper tw)
-		{
-			List<MethodWrapper> list = new List<MethodWrapper>();
-			foreach (MethodWrapper mw in tw.GetMethods())
-			{
-				if (mw.IsConstructor && !mw.IsHideFromReflection && !mw.IsPrivate)
-				{
-					list.Add(mw);
-				}
-			}
-			list.Sort(delegate(MethodWrapper mw1, MethodWrapper mw2) { return String.CompareOrdinal(mw1.Signature, mw2.Signature); });
-			foreach (MethodWrapper mw in list)
-			{
-				Modifiers mods = mw.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Synchronized | Modifiers.Native | Modifiers.Abstract | Modifiers.Strictfp);
-				bes.WriteUtf8(mw.Name);
-				bes.WriteUInt32((uint)mods);
-				bes.WriteUtf8(mw.Signature);
-			}
-		}
+        static void WriteInterfaces(BigEndianStream bes, TypeWrapper tw)
+        {
+            foreach (var i in tw.Interfaces.OrderBy(i => i.Name))
+                bes.WriteUtf8(i.Name);
+        }
 
-		private static void WriteMethods(BigEndianStream bes, TypeWrapper tw)
-		{
-			List<MethodWrapper> list = new List<MethodWrapper>();
-			foreach (MethodWrapper mw in tw.GetMethods())
-			{
-				if (!mw.IsConstructor && !mw.IsHideFromReflection && !mw.IsPrivate)
-				{
-					list.Add(mw);
-				}
-			}
-			list.Sort(delegate(MethodWrapper mw1, MethodWrapper mw2) {
-				if (mw1.Name == mw2.Name)
-					return String.CompareOrdinal(mw1.Signature, mw2.Signature);
-				return String.CompareOrdinal(mw1.Name, mw2.Name); 
-			});
-			foreach (MethodWrapper mw in list)
-			{
-				Modifiers mods = mw.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Synchronized | Modifiers.Native | Modifiers.Abstract | Modifiers.Strictfp);
-				bes.WriteUtf8(mw.Name);
-				bes.WriteUInt32((uint)mods);
-				bes.WriteUtf8(mw.Signature);
-			}
-		}
-	}
+        static void WriteFields(BigEndianStream bes, TypeWrapper tw)
+        {
+            foreach (var fw in tw.GetFields().Where(i => !i.IsHideFromReflection).OrderBy(i => i.Name))
+            {
+                var mods = fw.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Volatile | Modifiers.Transient);
+                if (((mods & Modifiers.Private) == 0) || ((mods & (Modifiers.Static | Modifiers.Transient)) == 0))
+                {
+                    bes.WriteUtf8(fw.Name);
+                    bes.WriteUInt32((uint)mods);
+                    bes.WriteUtf8(fw.Signature.Replace('.', '/'));
+                }
+            }
+        }
+
+        static void WriteStaticInitializer(BigEndianStream bes, TypeWrapper tw)
+        {
+            var type = tw.TypeAsTBD;
+            if (!type.IsArray && type.TypeInitializer != null)
+            {
+                if (!AttributeHelper.IsHideFromJava(type.TypeInitializer))
+                {
+                    bes.WriteUtf8("<clinit>");
+                    bes.WriteUInt32((uint)Modifiers.Static);
+                    bes.WriteUtf8("()V");
+                }
+            }
+        }
+
+        static void WriteConstructors(BigEndianStream bes, TypeWrapper tw)
+        {
+            var ctors = tw.GetMethods()
+                .Where(i => i.IsConstructor && !i.IsHideFromReflection && !i.IsPrivate)
+                .OrderBy(i => i.Signature);
+
+            foreach (var ctor in ctors)
+            {
+                var mods = ctor.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Synchronized | Modifiers.Native | Modifiers.Abstract | Modifiers.Strictfp);
+                bes.WriteUtf8(ctor.Name);
+                bes.WriteUInt32((uint)mods);
+                bes.WriteUtf8(ctor.Signature);
+            }
+        }
+
+        static void WriteMethods(BigEndianStream bes, TypeWrapper tw)
+        {
+            var methods = tw.GetMethods()
+                .Where(i => !i.IsConstructor && !i.IsHideFromReflection && !i.IsPrivate)
+                .OrderBy(i => i.Name)
+                .ThenBy(i => i.Signature);
+
+            foreach (var method in methods)
+            {
+                var mods = method.Modifiers & (Modifiers.Public | Modifiers.Private | Modifiers.Protected | Modifiers.Static | Modifiers.Final | Modifiers.Synchronized | Modifiers.Native | Modifiers.Abstract | Modifiers.Strictfp);
+                bes.WriteUtf8(method.Name);
+                bes.WriteUInt32((uint)mods);
+                bes.WriteUtf8(method.Signature);
+            }
+        }
+
+    }
+
 }
