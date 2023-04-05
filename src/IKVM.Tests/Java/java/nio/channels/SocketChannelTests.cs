186b0ea8c80171ea0d24e5e24d58286b515299ef
Jerome Haltom <jhaltom@alethic.solutions>
2023-04-05 15:02:41 -0500

    Same

---
+++ src/IKVM.Tests/Java/java/nio/channels/SocketChannelTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using java.net;
+using java.nio;
+using java.nio.channels;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.Java.java.nio.channels
+{
+
+    [TestClass]
+    public class SocketChannelTests
+    {
+
+        [TestMethod]
+        public async Task CanSendUrgentData()
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var receive = ByteBuffer.allocate(1024);
+
+            using var ssc = ServerSocketChannel.open();
+            ssc.bind(new InetSocketAddress(0));
+
+            var sst = Task.Run(() =>
+            {
+                // accept first connect
+                var sc = ssc.accept();
+
+                // read into receive buffer
+                while (sc.read(receive) != -1 && cancellationTokenSource.Token.IsCancellationRequested == false)
+                    continue;
+            });
+
+            // wait a bit and write some messages to the server
+            await Task.Delay(100);
+            using (var sc = SocketChannel.open(new InetSocketAddress(((InetSocketAddress)ssc.getLocalAddress()).getPort())))
+            {
+                sc.configureBlocking(false);
+                sc.socket().setOOBInline(false);
+
+                foreach (var i in new byte[] { 1, 2, 3, 4 })
+                {
+                    sc.socket().sendUrgentData(i);
+                    await Task.Delay(100);
+                }
+            }
+
+            cancellationTokenSource.Cancel();
+            await sst;
+        }
+
+    }
+
+}
