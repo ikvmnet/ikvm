443e72dd013edd60d86e557a6e51de810c7f2d6b
Jerome Haltom <jhaltom@alethic.solutions>
2023-04-11 13:47:37 -0500

    This test can't work anyways, since we have no way in C# to abort before catching a callback. Hmm.

--- src/IKVM.Tests/Java/java/lang/ObjectTests.cs
+++ src/IKVM.Tests/Java/java/lang/ObjectTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-
-using FluentAssertions;
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IKVM.Tests.Java.java.lang
 {
@@ -11,55 +7,7 @@ namespace IKVM.Tests.Java.java.lang
     public class ObjectTests
     {
 
-        class ClassWithFinalize : global::java.lang.Object
-        {
 
-            Action<ClassWithFinalize> onFinalize;
-
-            public ClassWithFinalize(Action<ClassWithFinalize> onFinalize)
-            {
-                this.onFinalize = onFinalize;
-                throw new Exception(); // abort ctor, never calling base
-            }
-
-            protected override void finalize()
-            {
-                onFinalize(this);
-            }
-
-            ~ClassWithFinalize()
-            {
-                // this should be hit
-            }
-
-        }
-
-        /// <summary>
-        /// JLS tells us that objects that have not completed java.lang.Object:init should not invoke finalize().
-        /// </summary>
-        [TestMethod]
-        public void AbortingConstructorShouldNotFinalize()
-        {
-            ClassWithFinalize o = null;
-
-            void Run()
-            {
-                try
-                {
-                    new ClassWithFinalize(t => o = t);
-                }
-                catch (Exception e)
-                {
-                    // ignore
-                }
-            }
-
-            Run();
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            o.Should().BeNull();
-        }
 
     }
 
