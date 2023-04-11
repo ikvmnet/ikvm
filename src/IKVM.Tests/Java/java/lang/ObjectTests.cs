375d73b312e8174a07381ffcd3104311e3b71459
Jerome Haltom <jhaltom@alethic.solutions>
2023-04-11 13:45:21 -0500

    Test that finalize() isn't invoked.

--- src/IKVM.Tests/Java/java/lang/ObjectTests.cs
+++ src/IKVM.Tests/Java/java/lang/ObjectTests.cs
@@ -1,90 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 using FluentAssertions;
-
-using IKVM.Java.Extensions.java.lang;
-using IKVM.Tests.Util;
-
-using java.lang;
-using java.util.function;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IKVM.Tests.Java.java.lang
 {
 
-    //[TestClass]
-    //public class ObjectTests
-//    {
+    [TestClass]
+    public class ObjectTests
+    {
 
-//        [TestMethod]
-//        public void CanOverrideFinalize()
-//        {
-//            var source = """
-//import java.lang.*;
-//import java.util.function.Consumer;
+        class ClassWithFinalize : global::java.lang.Object
+        {
 
-//public class ObjectWithFinalize {
+            Action<ClassWithFinalize> onFinalize;
 
-//    //public Runnable onFinalize;
+            public ClassWithFinalize(Action<ClassWithFinalize> onFinalize)
+            {
+                this.onFinalize = onFinalize;
+                throw new Exception(); // abort ctor, never calling base
+            }
 
-//    @Override
-//    protected void finalize() {
-//        System.out.println("finalize");
-//        //onFinalize.run();
-//    }
+            protected override void finalize()
+            {
+                onFinalize(this);
+            }
 
-//}
-//""";
-//            var unit = new InMemoryCodeUnit("ObjectWithFinalize", source);
-//            var compiler = new InMemoryCompiler(new[] { unit });
-//            compiler.Compile();
+            ~ClassWithFinalize()
+            {
+                // this should be hit
+            }
 
-//            // check the clazz
-//            var clazz = compiler.GetClass("ObjectWithFinalize");
-//            var ctor = clazz.getConstructor();
-//            var type = global::ikvm.runtime.Util.getRuntimeTypeFromClass(clazz);
-//            type.GetMethod("Finalize", BindingFlags.NonPublic | BindingFlags.Instance).Should().NotBeNull();
+        }
 
-//            // record when finalizer is executed
-//            var f = false;
-//            f.Should().BeFalse();
+        /// <summary>
+        /// JLS tells us that objects that have not completed java.lang.Object:init should not invoke finalize().
+        /// </summary>
+        [TestMethod]
+        public void AbortingConstructorShouldNotFinalize()
+        {
+            ClassWithFinalize o = null;
 
-//            // run on other method to prevent debug holding instance
-//            [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-//            static void CreateClassWithoutReference(Action action)
-//            {
-//                var test = (dynamic)ctor.newInstance(System.Array.Empty<object>());
-//                //test.onFinalize = new DelegateRunnable(action);
-//            }
+            void Run()
+            {
+                try
+                {
+                    new ClassWithFinalize(t => o = t);
+                }
+                catch (Exception e)
+                {
+                    // ignore
+                }
+            }
 
-//            CreateClassWithoutReference(() => f = true);
-//            GC.Collect();
-//            GC.WaitForPendingFinalizers();
-//            f.Should().BeTrue();
-//        }
+            Run();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
-//        [TestMethod]
-//        public void FinalizerWorks()
-//        {
-//            var obj = new Example();
-//            obj = null;    // Avoid debugger extending its lifetime
-//            GC.Collect();
-//            GC.WaitForPendingFinalizers();
-//            global::System.Console.WriteLine.ReadLine();
-//        }
+            o.Should().BeNull();
+        }
 
-//        class Base { ~Base() { global::System.Console.WriteLine("Base finalizer called"); } }
-//        class Derived : Base { ~Derived() { global::System.Console.WriteLine("Derived finalizer called"); } }
-//        class Example : Derived { }
-
-//    }
+    }
 
 }
