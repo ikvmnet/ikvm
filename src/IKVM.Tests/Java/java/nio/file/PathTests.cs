d53a35de6dc2b6992ec6f62c19bdd5ac6a740285
Jerome Haltom <jhaltom@alethic.solutions>
2023-04-28 11:39:53 -0500

    Merge tests

--- src/IKVM.Tests/Java/java/nio/file/PathTests.cs
+++ src/IKVM.Tests/Java/java/nio/file/PathTests.cs
@@ -14,29 +14,38 @@ namespace IKVM.Tests.Java.java.nio.file
     {
 
         [DataTestMethod]
-        [DataRow("foo", "C:\\foo")]
-        [DataRow("D:\\bar", "D:\\bar")]
-        [DataRow("\\\\server\\share\\bar", "\\\\server\\share\\bar")]
-        [DataRow("C:foo", "C:\\foo")]
-        [DataRow("D:foo", "D:foo")]
-        [DataRow("", "C:\\")]
-        public void CanResolveFromWindowsDrive(string other, string expected)
+        [DataRow("C:\\", "foo", "C:\\foo")]
+        [DataRow("C:\\", "D:\\bar", "D:\\bar")]
+        [DataRow("C:\\", "\\\\server\\share\\bar", "\\\\server\\share\\bar")]
+        [DataRow("C:\\", "C:foo", "C:\\foo")]
+        [DataRow("C:\\", "D:foo", "D:foo")]
+        [DataRow("C:\\", "", "C:\\")]
+        //[DataRow("C:", "foo", "C:foo")]
+        [DataRow("C:", "", "C:")]
+        public void CanResolveWindowsPaths(string path, string other, string expected)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
                 return;
 
-            Paths.get("C:\\").resolve(other).ToString().Should().Be(expected);
+            Paths.get(path).resolve(other).ToString().Should().Be(expected);
         }
 
         [DataTestMethod]
-        //[DataRow("foo", "C:foo")] currently broken
-        [DataRow("", "C:")]
-        public void CanResolveFromWindowsDriveName(string other, string expected)
+        [DataRow("/tmp", "foo", "/tmp/foo")]
+        [DataRow("/tmp", "/foo", "/foo")]
+        [DataRow("/tmp", "", "/tmp")]
+        [DataRow("tmp", "foo", "tmp/foo")]
+        [DataRow("tmp", "/foo", "/foo")]
+        [DataRow("tmp", "", "tmp")]
+        [DataRow("", "", "")]
+        [DataRow("", "foo", "foo")]
+        [DataRow("", "/foo", "/foo")]
+        public void CanResolveUnixPaths(string path, string other, string expected)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                 return;
 
-            Paths.get("C:").resolve(other).ToString().Should().Be(expected);
+            Paths.get(path).resolve(other).ToString().Should().Be(expected);
         }
 
     }
