2a1c96220dd21226c830e61a1e9295b5f01d014f
Jerome Haltom <jhaltom@alethic.solutions>
2023-04-28 10:24:40 -0500

    Added some tests for path construction. More to include.

---
+++ src/IKVM.Tests/Java/java/nio/file/PathTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+using FluentAssertions;
+
+using java.nio.file;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.Java.java.nio.file
+{
+
+    [TestClass]
+    public class PathTests
+    {
+
+        [DataTestMethod]
+        [DataRow("foo", "C:\\foo")]
+        [DataRow("D:\\bar", "D:\\bar")]
+        [DataRow("\\\\server\\share\\bar", "\\\\server\\share\\bar")]
+        [DataRow("C:foo", "C:\\foo")]
+        [DataRow("D:foo", "D:foo")]
+        [DataRow("", "C:\\")]
+        public void CanResolveFromWindowsDrive(string other, string expected)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
+                return;
+
+            Paths.get("C:\\").resolve(other).ToString().Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        //[DataRow("foo", "C:foo")] currently broken
+        [DataRow("", "C:")]
+        public void CanResolveFromWindowsDriveName(string other, string expected)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
+                return;
+
+            Paths.get("C:").resolve(other).ToString().Should().Be(expected);
+        }
+
+    }
+
+}
