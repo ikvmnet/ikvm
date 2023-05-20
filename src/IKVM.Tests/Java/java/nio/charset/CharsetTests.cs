aa38db357502390d491ce12fd4e286f0cdf6e059
Jerome Haltom <jhaltom@alethic.solutions>
2023-05-20 17:13:59 -0500

    Few more tests

--- src/IKVM.Tests/Java/java/nio/charset/CharsetTests.cs
+++ src/IKVM.Tests/Java/java/nio/charset/CharsetTests.cs
@@ -1,4 +1,6 @@
-﻿using java.nio.charset;
+﻿using FluentAssertions;
+
+using java.nio.charset;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,9 +12,36 @@ namespace IKVM.Tests.Java.java.nio.charset
     {
 
         [TestMethod]
+        public void CanGetDefaultCharsets()
+        {
+            Charset.defaultCharset();
+        }
+
+        [TestMethod]
         public void CanGetAvailableCharsets()
         {
             Charset.availableCharsets();
+        }
+
+        [DataTestMethod]
+        [DataRow("cp437")]
+        [DataRow("ms850")]
+        [DataRow("ms852")]
+        [DataRow("ms855")]
+        [DataRow("ms857")]
+        [DataRow("ms860")]
+        [DataRow("ms861")]
+        [DataRow("ms863")]
+        [DataRow("ms865")]
+        [DataRow("ms866")]
+        [DataRow("ms869")]
+        [DataRow("ms936")]
+        [DataRow("cp65001")]
+        public void CanEncodeAndDecode(string charset)
+        {
+            var o = Charset.forName(charset).encode("test");
+            var i = Charset.forName(charset).decode(o);
+            i.Should().Be("test");
         }
 
     }
