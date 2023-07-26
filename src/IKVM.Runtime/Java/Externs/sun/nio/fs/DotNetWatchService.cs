9481a6e80a888d209298bc18ffb18841a09845b6
AliveDevil <AliveDevil@users.noreply.github.com>
2023-07-26 21:35:41 +0200

    Format

--- src/IKVM.Runtime/Java/Externs/sun/nio/fs/DotNetWatchService.cs
+++ src/IKVM.Runtime/Java/Externs/sun/nio/fs/DotNetWatchService.cs
@@ -1,3 +1,4 @@
+using System;
 using IKVM.Runtime.Accessors.Sun.Nio.Fs;
 using usnfs = IKVM.Runtime.Util.Sun.Nio.Fs;
 
@@ -6,29 +7,24 @@ namespace IKVM.Java.Externs.sun.nio.fs
     internal static partial class DotNetWatchService
     {
 
-        public static void close0(object key) => close0Impl(key);
-
-        public static void register0(object fs, object key, object dir, object[] events, params object[] modifiers) => register0Impl(fs, key, dir, events, modifiers);
-
-        static partial void close0Impl(object key);
-
-        static partial void register0Impl(object fs, object key, object dir, object[] events, object[] modifiers);
-
-    }
-
-#if !FIRST_PASS
-
-    internal static partial class DotNetWatchService
-    {
-
-        static partial void close0Impl(object key)
-            => usnfs.DotNetWatchService.Close(key);
-
-        static partial void register0Impl(object fs, object key, object dir, object[] events, object[] modifiers)
-            => usnfs.DotNetWatchService.Register(fs, key, dir, events, modifiers);
-
-    }
-
+        public static void close0(object key)
+        {
+#if FIRST_PASS
+            throw new NotImplementedException();
+#else
+            usnfs.DotNetWatchService.Close(key);
 #endif
+        }
+
+        public static void register0(object fs, object key, object dir, object[] events, params object[] modifiers)
+        {
+#if FIRST_PASS
+            throw new NotImplementedException();
+#else
+            usnfs.DotNetWatchService.Register(fs, key, dir, events, modifiers);
+#endif
+        }
+
+    }
 
 }
