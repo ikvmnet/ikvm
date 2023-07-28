53125744822fe24f56c98c0a9480372b49b477c6
AliveDevil <AliveDevil@users.noreply.github.com>
2023-07-28 18:56:05 +0200

    Fix sig

--- src/IKVM.Runtime/Accessors/Sun/Nio/Fs/DotNetWatchServiceKeyAccessor.cs
+++ src/IKVM.Runtime/Accessors/Sun/Nio/Fs/DotNetWatchServiceKeyAccessor.cs
@@ -26,10 +26,10 @@ namespace IKVM.Runtime.Accessors.Sun.Nio.Fs
         }
 
         public void error(object self)
-            => (error0 ??= GetMethod(ref error0, nameof(error), Void, default)).Invoker(self);
+            => (error0 ??= GetMethod(ref error0, nameof(error), Void)).Invoker(self);
 
         public void signalEvent(object self)
-            => (signalEvent0 ??= GetMethod(ref signalEvent0, nameof(signalEvent), Void, default)).Invoker(self);
+            => (signalEvent0 ??= GetMethod(ref signalEvent0, nameof(signalEvent), Void)).Invoker(self);
 
         public void signalEvent(object self, object kind, object context)
             => (signalEvent1 ??= GetMethod(ref signalEvent1, nameof(signalEvent), Void, WatchEventKind, typeof(object))).Invoker(self, kind, context);
