9f0c8a59e92b71a9ae271383c224873f32a39f10
Jerome Haltom <jhaltom@alethic.solutions>
2023-08-02 11:51:28 -0500

    Load deps from util. Not quite right still. Really should be using refs, but mscorlib is missing. Have to figure this out.

--- src/IKVM.Tests/Java/ikvm/runtime/AssemblyClassLoaderTests.cs
+++ src/IKVM.Tests/Java/ikvm/runtime/AssemblyClassLoaderTests.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 using FluentAssertions;
 
+using IKVM.Runtime.Extensions;
+using IKVM.Tests.Util;
 using IKVM.Tools.Runner;
 using IKVM.Tools.Runner.Importer;
 
@@ -19,6 +22,8 @@ namespace IKVM.Tests.Java.ikvm.runtime
     public class AssemblyClassLoaderTests
     {
 
+        static readonly string TESTBASE = Path.GetDirectoryName(typeof(AssemblyClassLoaderTests).Assembly.Location);
+
         Assembly helloworldDll;
 
         public TestContext TestContext { get; set; }
@@ -30,13 +35,22 @@ namespace IKVM.Tests.Java.ikvm.runtime
         [TestInitialize]
         public async Task Setup()
         {
-#if NETCOREAPP
-            var toolFramework = "net6.0";
-#else
-            var toolFramework = "net472";
+#if NET7_0
+            var ikvmTool = "net6.0";
+            var ikvmLibs = Path.Combine(TESTBASE, "lib", "net6.0");
+            var libPaths = DotNetSdkUtil.GetPathToReferenceAssemblies("net7.0", ".NETCore", "7.0");
+#endif
+#if NET6_0       
+            var ikvmTool = "net6.0";
+            var ikvmLibs = Path.Combine(TESTBASE, "lib", "net6.0");
+            var libPaths = DotNetSdkUtil.GetPathToReferenceAssemblies("net6.0", ".NETCore", "6.0");
+#endif
+#if NET461
+            var ikvmTool = "net472";
+            var ikvmLibs = Path.Combine(TESTBASE, "lib", "net461");
+            var libPaths = DotNetSdkUtil.GetPathToReferenceAssemblies("net461", ".NETFramework", "4.6.1");
 #endif
 
-            var refsDir = Path.Combine(Path.GetDirectoryName(typeof(AssemblyClassLoaderTests).Assembly.Location), "refs");
             var n = Guid.NewGuid().ToString("n");
             var p = Path.Combine(Path.GetTempPath(), n, $"helloworld_{n}.dll");
             Directory.CreateDirectory(Path.GetDirectoryName(p));
@@ -46,22 +60,23 @@ namespace IKVM.Tests.Java.ikvm.runtime
                 rid = "win7-x64";
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
                 rid = "linux-x64";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                rid = "osx-x64";
 
             var e = new List<IkvmToolDiagnosticEvent>();
-            var l = new IkvmImporterLauncher(Path.Combine(Path.GetDirectoryName(typeof(AssemblyClassLoaderTests).Assembly.Location), "ikvmc", toolFramework, rid), new IkvmToolDelegateDiagnosticListener(evt => { e.Add(evt); TestContext.WriteLine(evt.Message, evt.MessageArgs); }));
+            var l = new IkvmImporterLauncher(Path.Combine(Path.GetDirectoryName(typeof(AssemblyClassLoaderTests).Assembly.Location), "ikvmc", ikvmTool, rid), new IkvmToolDelegateDiagnosticListener(evt => { e.Add(evt); TestContext.WriteLine(evt.Message, evt.MessageArgs); }));
+            var z = libPaths.Append(ikvmLibs).ToArray();
             var o = new IkvmImporterOptions()
             {
-                Runtime = Path.Combine(refsDir, "IKVM.Runtime.dll"),
+                Runtime = Path.Combine(ikvmLibs, "IKVM.Runtime.dll"),
                 ResponseFile = $"{n}_ikvmc.rsp",
                 Input = { Path.Combine("helloworld", "helloworld-2.0.jar") },
                 Assembly = $"helloworld_{n}",
                 Version = "1.0.0.0",
                 NoStdLib = true,
                 Output = p,
+                Lib = z
             };
-
-            foreach (var dll in Directory.GetFiles(refsDir, "*.dll"))
-                o.References.Add(dll);
 
             var exitCode = await l.ExecuteAsync(o);
             exitCode.Should().Be(0);
@@ -70,7 +85,7 @@ namespace IKVM.Tests.Java.ikvm.runtime
         }
 
         [TestMethod]
-        public void Can_get_package()
+        public void CanGetPackage()
         {
             var t = helloworldDll.GetType("sample.HelloworldImpl");
             var k = ((global::java.lang.Class)t).getPackage();
@@ -78,7 +93,7 @@ namespace IKVM.Tests.Java.ikvm.runtime
         }
 
         [TestMethod]
-        public void Can_get_resource()
+        public void CanGetResource()
         {
             var t = helloworldDll.GetType("sample.HelloworldImpl");
             var k = ((global::java.lang.Class)t).getResource("/helloworld.composite");
