c5b3e8cd23688f9aa3529e201560877816d1da32
Jerome Haltom <jhaltom@alethic.solutions>
2023-09-14 14:59:20 -0500

    Add some network interface tests.

---
+++ src/IKVM.Tests/Java/java/net/NetworkInterfaceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using FluentAssertions;
+
+using java.net;
+using java.util;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.Java.java.net
+{
+
+    [TestClass]
+    public class NetworkInterfaceTests
+    {
+
+        [TestMethod]
+        public void ShouldHaveAtLeast1NetworkInterface()
+        {
+            NetworkInterface.getNetworkInterfaces().AsEnumerable<NetworkInterface>().ToList().Should().HaveCountGreaterThan(0);
+        }
+
+    }
+
+}
