bfcc9ba1ee7553e542384dd01d99728fc7b3e276
Jerome Haltom <jhaltom@alethic.solutions>
2023-10-11 22:39:59 -0500

    Disable test for unix.

--- src/IKVM.MSBuild.Tasks.Tests/IkvmReferenceExportItemPrepareTests.cs
+++ src/IKVM.MSBuild.Tasks.Tests/IkvmReferenceExportItemPrepareTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 
 using FluentAssertions;
 
@@ -23,6 +24,10 @@ namespace IKVM.MSBuild.Tasks.Tests
         [DataRow("net6.0", ".NETCore", "6.0")]
         public void CanPrepare(string tfm, string targetFrameworkIdentifier, string targetFrameworkVersion)
         {
+            // no reference assemblies for NetFX on Unix
+            if (tfm == "net472" && RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
+                return;
+
             var engine = new Mock<IBuildEngine7>();
             var errors = new List<BuildErrorEventArgs>();
             engine.Setup(x => x.LogErrorEvent(It.IsAny<BuildErrorEventArgs>())).Callback((BuildErrorEventArgs e) => errors.Add(e));
