04c8c5af8b1c02c46810e74e45f697f1e7570819
Jerome Haltom <jhaltom@alethic.solutions>
2024-01-09 17:56:44 -0600

    Core test.

--- src/IKVM.Reflection.Tests/ModuleWriterTests.cs
+++ src/IKVM.Reflection.Tests/ModuleWriterTests.cs
@@ -16,7 +16,7 @@ namespace IKVM.Reflection.Tests
         [TestMethod]
         public void CanWriteNetFxModule()
         {
-            var d = Path.Combine(TestContext.TestRunResultsDirectory, "IKVK.Reflection.Tests", "ModuleWriterTests");
+            var d = Path.Combine(TestContext.TestRunResultsDirectory, "IKVM.Reflection.Tests", "ModuleWriterTests");
             if (Directory.Exists(d))
                 Directory.Delete(d, true);
             Directory.CreateDirectory(d);
@@ -24,6 +24,22 @@ namespace IKVM.Reflection.Tests
             var u = new Universe("mscorlib");
             var a = u.DefineDynamicAssembly(new AssemblyName("Test"), AssemblyBuilderAccess.Save, d);
             var m = a.DefineDynamicModule("Test", "Test.dll", false);
+            m.DefineType("Type");
+            a.Save("Test.dll");
+        }
+
+        [TestMethod]
+        public void CanWriteNetCoreModule()
+        {
+            var d = Path.Combine(TestContext.TestRunResultsDirectory, "IKVM.Reflection.Tests", "ModuleWriterTests");
+            if (Directory.Exists(d))
+                Directory.Delete(d, true);
+            Directory.CreateDirectory(d);
+
+            var u = new Universe("System.Runtime");
+            var a = u.DefineDynamicAssembly(new AssemblyName("Test"), AssemblyBuilderAccess.Save, d);
+            var m = a.DefineDynamicModule("Test", "Test.dll", false);
+            m.DefineType("Type");
             a.Save("Test.dll");
         }
 
