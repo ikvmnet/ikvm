567957d9dafd204a84b0b1f70246e24c8ed559d5
Jerome Haltom <jhaltom@alethic.solutions>
2024-02-06 10:26:23 -0600

    Remove debug tables. Add some tests.

--- src/IKVM.Reflection/Module.cs
+++ src/IKVM.Reflection/Module.cs
@@ -74,12 +74,6 @@ namespace IKVM.Reflection
         internal readonly GenericParamTable GenericParamTable = new GenericParamTable();
         internal readonly MethodSpecTable MethodSpecTable = new MethodSpecTable();
         internal readonly GenericParamConstraintTable GenericParamConstraint = new GenericParamConstraintTable();
-        internal readonly DocumentTable DocumentTable = new DocumentTable();
-        internal readonly MethodDebugInformationTable MethodDebugInformationTable = new MethodDebugInformationTable();
-        internal readonly LocalScopeTable LocalScopeTable = new LocalScopeTable();
-        internal readonly LocalVariableTable LocalVariableTable = new LocalVariableTable();
-        internal readonly LocalConstantTable LocalConstantTable = new LocalConstantTable();
-        internal readonly ImportScopeTable ImportScopeTable = new ImportScopeTable();
 
         /// <summary>
         /// Initializes a new instance.
@@ -137,12 +131,6 @@ namespace IKVM.Reflection
             tables[GenericParamTable.Index] = GenericParamTable;
             tables[MethodSpecTable.Index] = MethodSpecTable;
             tables[GenericParamConstraintTable.Index] = GenericParamConstraint;
-            tables[DocumentTable.Index] = DocumentTable;
-            tables[MethodDebugInformationTable.Index] = MethodDebugInformationTable;
-            tables[LocalScopeTable.Index] = LocalScopeTable;
-            tables[LocalVariableTable.Index] = LocalVariableTable;
-            tables[LocalConstantTable.Index] = LocalConstantTable;
-            tables[ImportScopeTable.Index] = ImportScopeTable;
             return tables;
         }
 
