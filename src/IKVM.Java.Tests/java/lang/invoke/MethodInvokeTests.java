e58b9ea6f4240deb6935aef58baddd5bc6032b77
Jerome Haltom <jhaltom@alethic.solutions>
2024-02-12 15:50:06 -0600

    Add a test to see if method invoke throws the right thing.

---
+++ src/IKVM.Java.Tests/java/lang/invoke/MethodInvokeTests.java
@@ -0,0 +1,31 @@
+package ikvm.tests.java.java.lang.invoke;
+
+import java.lang.*;
+import java.lang.invoke.*;
+import java.util.*;
+
+@cli.Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute.Annotation()
+public class MethodInvokeTests {
+    
+    public static void switchWithNoCases(String s) {
+        switch (s) { }
+    }
+    
+    @cli.Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute.Annotation()
+    public void switchWithNullShouldThrowNullPointerExceptionInInvocationTargetException() throws Throwable {
+        Method method = MethodInvokeTests.class.getDeclaredMethod("switchWithNoCases", String.class);
+
+        try {
+            method.invoke(null, (String)null);
+        } catch (InvocationTargetException ite) {
+            if (ite.getTargetException() instanceof NullPointerException) {
+                return;
+            } else {
+                throw new RuntimeException("Expected exception not thrown.", ite);
+            }
+        }
+
+        throw new RuntimeException("Expected exception not thrown.");
+    }
+
+}
