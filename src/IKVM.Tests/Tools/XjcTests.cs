1869bf9496cda248c545a18c7dfd91b38f6274b1
Jerome Haltom <jhaltom@alethic.solutions>
2024-02-26 21:32:47 -0600

    Set as executable.

--- src/IKVM.Tests/Tools/XjcTests.cs
+++ src/IKVM.Tests/Tools/XjcTests.cs
@@ -21,6 +21,18 @@ namespace IKVM.Tests.Tools
         {
             var s = new StringBuilder();
             var c = Path.Combine(java.lang.System.getProperty("java.home"), "bin", "xjc");
+#if NETCOREAPP
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                var psx = Mono.Unix.UnixFileSystemInfo.GetFileSystemEntry(c);
+                var prm = psx.FileAccessPermissions;
+                prm |= Mono.Unix.FileAccessPermissions.UserExecute;
+                prm |= Mono.Unix.FileAccessPermissions.GroupExecute;
+                prm |= Mono.Unix.FileAccessPermissions.OtherExecute;
+                if (prm != psx.FileAccessPermissions)
+                    psx.FileAccessPermissions = prm;
+            }
+#endif
             var r = await Cli.Wrap(c).WithArguments("-help").WithStandardOutputPipe(PipeTarget.ToDelegate(i => s.Append(i))).WithValidation(CommandResultValidation.None).ExecuteAsync();
             r.ExitCode.Should().Be(RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? -1 : 255);
             s.ToString().Should().StartWith("Usage: xjc");
