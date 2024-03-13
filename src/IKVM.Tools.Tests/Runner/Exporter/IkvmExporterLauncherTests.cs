2c4d659cf8effbfc71c0e12373b7a577f8810fed
Jerome Haltom <jhaltom@alethic.solutions>
2024-03-12 20:41:50 -0500

    Rename a bunch of net461 folders to net472. Remove net472 support for non-Windows. The only usage of this could be Mono, and IKVM is broken on Mono anyways. This should reduce file size. Adjusted some tests to better test framework combinations. xjc wasn't set to self contained. Remove net472 and net6.0 tools. At this point, ikvmc and ikvmstub both are distributed self-contained with .NET 8, and are capable of fully producing down level assemblies. Thus, the requirement to have specific non-Core versions of these tools should be gone.

--- src/IKVM.Tools.Tests/Runner/Exporter/IkvmExporterLauncherTests.cs
+++ src/IKVM.Tools.Tests/Runner/Exporter/IkvmExporterLauncherTests.cs
@@ -26,10 +26,12 @@ namespace IKVM.Tools.Tests.Runner.Exporter
         [DataRow("net472", "net472", "net472", ".NETFramework", "4.7.2")]
         [DataRow("net472", "net472", "net481", ".NETFramework", "4.8.1")]
         [DataRow("net472", "net6.0", "net6.0", ".NETCore", "6.0")]
-        [DataRow("net6.0", "net472", "net472", ".NETFramework", "4.7.2")]
-        [DataRow("net6.0", "net472", "net481", ".NETFramework", "4.8.1")]
-        [DataRow("net6.0", "net6.0", "net6.0", ".NETCore", "6.0")]
-        [DataRow("net6.0", "net6.0", "net7.0", ".NETCore", "7.0")]
+        [DataRow("net8.0", "net472", "net472", ".NETFramework", "4.7.2")]
+        [DataRow("net8.0", "net472", "net481", ".NETFramework", "4.8.1")]
+        [DataRow("net8.0", "net6.0", "net6.0", ".NETCore", "6.0")]
+        [DataRow("net8.0", "net6.0", "net7.0", ".NETCore", "7.0")]
+        [DataRow("net8.0", "net6.0", "net8.0", ".NETCore", "8.0")]
+        [DataRow("net8.0", "net8.0", "net8.0", ".NETCore", "8.0")]
         public async System.Threading.Tasks.Task CanExportDll(string toolFramework, string ikvmFramework, string targetFramework, string targetFrameworkIdentifier, string targetFrameworkVersion)
         {
             if (toolFramework == "net472" && RuntimeInformation.IsOSPlatform(OSPlatform.Windows) == false)
