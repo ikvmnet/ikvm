264f9c612839f359bd15879edd08f9215e982ae0
Jerome Haltom <jhaltom@alethic.solutions>
2024-03-14 19:56:21 -0500

    Add ability to define ExcludeLists and IncludeLists in configuraton.

--- src/IKVM.JTReg.TestAdapter/JTRegTestOptionsExtensions.cs
+++ src/IKVM.JTReg.TestAdapter/JTRegTestOptionsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Linq;
+using System.Xml.Linq;
 
 using IKVM.JTReg.TestAdapter.Core;
 
@@ -10,12 +11,44 @@ namespace IKVM.JTReg.TestAdapter
     public static class JTRegTestOptionsExtensions
     {
 
+        const string JTRegConfigurationElementName = "JTRegConfiguration";
+        const string PartitionCountElementName = "PartitionCount";
+        const string TimeoutFactorElementName = "TimeoutFactor";
+        const string ExcludeListFilesElementName = "ExcludeListFiles";
+        const string IncludeListFilesElementName = "IncludeListFiles";
+
+        /// <summary>
+        /// Returns the <see cref="JTRegTestOptions"/> loaded from the specified <see cref="IRunSettings"/>.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
         public static JTRegTestOptions ToJTRegOptions(this IRunSettings self)
         {
-            var x = self?.SettingsXml != null ? XDocument.Parse(self.SettingsXml)?.Root?.Element("JTRegConfiguration") : null;
+            var x = self?.SettingsXml != null ? XDocument.Parse(self.SettingsXml)?.Root?.Element(JTRegConfigurationElementName) : null;
             var o = new JTRegTestOptions();
-            o.PartitionCount = (int?)x?.Element("PartitionCount") ?? o.PartitionCount;
-            o.TimeoutFactor = (float?)x?.Element("TimeoutFactor") ?? o.TimeoutFactor;
+            if (x != null)
+            {
+                if ((int?)x.Element(PartitionCountElementName) is int partitionCount)
+                    o.PartitionCount = partitionCount;
+
+                if ((float?)x.Element(TimeoutFactorElementName) is float timeoutFactor)
+                    o.TimeoutFactor = timeoutFactor;
+
+                var excludeListFilesElements = x.Elements(ExcludeListFilesElementName);
+                if (excludeListFilesElements != null && excludeListFilesElements.Any())
+                {
+                    o.ExcludeListFiles.Clear();
+                    o.ExcludeListFiles.AddRange(excludeListFilesElements.Select(i => i.Value));
+                }
+
+                var includeListFilesElements = x.Elements(IncludeListFilesElementName);
+                if (includeListFilesElements != null && includeListFilesElements.Any())
+                {
+                    o.IncludeListFiles.Clear();
+                    o.IncludeListFiles.AddRange(includeListFilesElements.Select(i => i.Value));
+                }
+            }
+
             return o;
         }
 
