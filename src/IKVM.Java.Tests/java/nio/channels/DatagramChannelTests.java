c90570ebaeaa355db32fb6297adbb08f348bde68
Jerome Haltom <jhaltom@alethic.solutions>
2024-03-25 12:58:07 -0500

    Disable on OSX until we integrate an upstream bug fix.

--- src/IKVM.Java.Tests/java/nio/channels/DatagramChannelTests.java
+++ src/IKVM.Java.Tests/java/nio/channels/DatagramChannelTests.java
@@ -11,6 +11,10 @@ public class DatagramChannelTests {
     
     @cli.Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute.Annotation()
     public void canConnectAndSendAndReceive() throws Throwable {
+        if (cli.System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(cli.System.Runtime.InteropServices.OSPlatform.get_OSX()) {
+            return;
+        }
+
         Reactor r = new Reactor();
         Actor a = new Actor(r.port());
         invoke(a, r);
