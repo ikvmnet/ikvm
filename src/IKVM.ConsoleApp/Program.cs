002473ed03f36bd447557aed2285259615e19698
Jerome Haltom <jhaltom@alethic.solutions>
2024-03-30 15:51:29 -0500

    Alright, new strategy. ModuleInitializers aren't going to work. We'll leave ikvm.lang.ModuleInitializer because it's neat though. Instead, distribute net472 win assemblies in lib/net472, while non-Windows go in runtimes/. This means by default net472 builds include Windows assemblies by default. Bank on a future capability of Mono of redirecting assemblies by OS. Maybe we'll be the ones to implement that.

--- src/IKVM.ConsoleApp/Program.cs
+++ src/IKVM.ConsoleApp/Program.cs
@@ -1,15 +1,23 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace IKVM.ConsoleApp
 {
+
     public static class Program
     {
 
+        [ModuleInitializer]
+        internal static void Foo()
+        {
+            Console.WriteLine("hi");
+        }
+
         public static void Main(string[] args)
         {
-            while (Debugger.IsAttached == false)
-                Thread.Sleep(1000);
+            Console.WriteLine("main");
         }
 
     }
--- src/IKVM.Runtime/RuntimeInit.cs
+++ src/IKVM.Runtime/RuntimeInit.cs
@@ -17,8 +17,6 @@ namespace IKVM.Runtime
 
 #if FIRST_PASS == false && IMPORTER == false && EXPORTER == false
 
-        const string BASE_ASSEMBLY_NAME = "IKVM.Java";
-
         /// <summary>
         /// Invoked on module initialize to load the JVM.
         /// </summary>
@@ -27,83 +25,11 @@ namespace IKVM.Runtime
 #pragma warning restore CA2255
         internal static void Init()
         {
-#if NETFRAMEWORK
-            // in the case of a framework-dependent deployment on NetFX, we need to discover IKVM.Java ourselves
-            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
-#endif
-
             // if our entry point is a Java application it will initialize the JVM as part of the launcher
             var isJavaApp = Assembly.GetEntryAssembly()?.ManifestModule?.GetCustomAttributes<JavaModuleAttribute>().Any() ?? false;
             if (isJavaApp == false)
                 JVM.Init();
         }
-
-#if NETFRAMEWORK
-
-        /// <summary>
-        /// Handles the <see cref="AppDomain.AssemblyResolve"/> event.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            if (args.Name == BASE_ASSEMBLY_NAME || args.Name.StartsWith(BASE_ASSEMBLY_NAME) && args.Name.StartsWith($"{BASE_ASSEMBLY_NAME}, "))
-                return LoadBaseAssembly();
-
-            return null;
-        }
-
-        /// <summary>
-        /// Attempts to resolve the given Java assembly.
-        /// </summary>
-        /// <returns></returns>
-        static Assembly LoadBaseAssembly()
-        {
-            // check if already loaded
-            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
-                if (asm.GetName().Name == BASE_ASSEMBLY_NAME)
-                    return asm;
-
-            // start looking at assembly path, or path given by environmental variable
-            var asml = typeof(RuntimeInit).Assembly.Location is string s ? Path.GetDirectoryName(s) : null;
-            var root = Environment.GetEnvironmentVariable("IKVM_PLATFORM_PATH") ?? asml;
-
-            // assembly possible loaded in memory: we have no available search path
-            if (string.IsNullOrEmpty(root))
-                return null;
-
-            // scan supported RIDs for current platform
-            foreach (var rid in RuntimeUtil.SupportedRuntimeIdentifiers)
-                if (Path.Combine(root, "runtimes", rid, "lib", "net472", $"{BASE_ASSEMBLY_NAME}.dll") is string path2)
-                    if (File.Exists(path2) && LoadAssembly(path2) is Assembly asm2)
-                        return asm2;
-
-            // scan supported RIDs for current platform
-            foreach (var rid in RuntimeUtil.SupportedRuntimeIdentifiers)
-                if (Path.Combine(root, "runtimes", rid, "lib", $"{BASE_ASSEMBLY_NAME}.dll") is string path2)
-                    if (File.Exists(path2) && LoadAssembly(path2) is Assembly asm2)
-                        return asm2;
-
-            // check in root directory
-            if (Path.Combine(root, $"{BASE_ASSEMBLY_NAME}.dll") is string path1)
-                if (File.Exists(path1) && LoadAssembly(path1) is Assembly asm1)
-                    return asm1;
-
-            return null;
-        }
-
-        /// <summary>
-        /// Loads the assembly at the given path.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        static Assembly LoadAssembly(string path)
-        {
-            return Assembly.LoadFrom(path);
-        }
-
-#endif
 
 #endif
 
