09c37b6d08a6c479e3e361076bb2f5b5a6a8859b
Jerome Haltom <jhaltom@alethic.solutions>
2024-04-03 09:35:56 -0500

    Move tests.

---
+++ src/IKVM.Tests/Runtime/Extensions/MemoryMarshalExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using IKVM.Runtime.Extensions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.Runtime.Extensions
+{
+
+    [TestClass]
+    public class MemoryMarshalExtensionsTests
+    {
+
+        [TestMethod]
+        public unsafe void CanFindNullByte()
+        {
+            fixed (byte* ptr = new byte[] { 0x01, 0x00 })
+                MemoryMarshalExtensions.GetIndexOfNull(ptr).Should().Be(1);
+            fixed (byte* ptr = new byte[] { 0x00, 0x00 })
+                MemoryMarshalExtensions.GetIndexOfNull(ptr).Should().Be(0);
+            fixed (byte* ptr = new byte[] { 0x01, 0x01, 0x00 })
+                MemoryMarshalExtensions.GetIndexOfNull(ptr).Should().Be(2);
+        }
+
+    }
+
+}
