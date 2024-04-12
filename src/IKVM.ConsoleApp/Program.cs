2a5da32450d38fe5efe995d3569b68b0ec9e9a72
Jerome Haltom <jhaltom@alethic.solutions>
2024-04-12 17:57:29 -0500

    More libs building.

--- src/IKVM.ConsoleApp/Program.cs
+++ src/IKVM.ConsoleApp/Program.cs
@@ -1,23 +1,44 @@
-﻿using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Threading;
+﻿using java.awt;
 
 namespace IKVM.ConsoleApp
 {
 
     public static class Program
     {
-
-        [ModuleInitializer]
-        internal static void Foo()
+        public class AWTExample1 : Frame
         {
-            Console.WriteLine("hi");
-        }
 
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("main");
+            // initializing using constructor  
+            AWTExample1()
+            {
+                // creating a button   
+                Button b = new Button("Click Me!!");
+
+                // setting button position on screen  
+                b.setBounds(30, 100, 80, 30);
+
+                // adding button into frame    
+                add(b);
+
+                // frame size 300 width and 300 height    
+                setSize(300, 300);
+
+                // setting the title of Frame  
+                setTitle("This is our basic AWT example");
+
+                // no layout manager   
+                setLayout(null);
+
+                // now frame will be visible, by default it is not visible    
+                setVisible(true);
+            }
+
+            public static void Main(string[] args)
+            {
+                // creating instance of Frame class   
+                AWTExample1 f = new AWTExample1();
+            }
+
         }
 
     }
