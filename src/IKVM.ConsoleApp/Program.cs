db911048ea5b8ddd65def89bff711110cdb377b5
Jerome Haltom <jhaltom@alethic.solutions>
2024-04-21 21:39:39 -0500

    Fix.

--- src/IKVM.ConsoleApp/Program.cs
+++ src/IKVM.ConsoleApp/Program.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Threading;
 
 namespace IKVM.ConsoleApp
 {
 
     public static class Program
     {
-
-        [ModuleInitializer]
-        internal static void Foo()
-        {
-            Console.WriteLine("hi");
-        }
 
         public static void Main(string[] args)
         {
