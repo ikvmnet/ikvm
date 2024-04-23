be1a01fd1d551cb2d579c33e73cea77d63c630c5
Jerome Haltom <jhaltom@alethic.solutions>
2024-04-23 11:21:24 -0500

    Override OSEnvironment.

---
+++ src/IKVM.Java/local/sun/misc/OSEnvironment.java
@@ -0,0 +1,12 @@
+// Licensed to the IKVM project under one or more agreements.
+// The IKVM project licenses this file to you under the Zlib license.
+
+package sun.misc;
+
+public class OSEnvironment {
+
+    public static void initialize() {
+
+    }
+
+}
