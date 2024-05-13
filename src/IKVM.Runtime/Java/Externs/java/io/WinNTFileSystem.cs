f4798c663dc6725cccdb9e2b3b1cfbb43db314f5
Jerome Haltom <jhaltom@alethic.solutions>
2024-05-12 21:31:36 -0500

    Wrong function.

--- src/IKVM.Runtime/Java/Externs/java/io/WinNTFileSystem.cs
+++ src/IKVM.Runtime/Java/Externs/java/io/WinNTFileSystem.cs
@@ -668,7 +668,7 @@ namespace IKVM.Java.Externs.java.io
             else
             {
                 __callerID ??= global::ikvm.@internal.CallerID.create(WinNTFileSystemAccessor.Type.TypeHandle);
-                __jniPtr__setReadOnly ??= Marshal.GetDelegateForFunctionPointer<__jniDelegate__setReadOnly>(JNIFrame.GetFuncPtr(__callerID, "java/io/WinNTFileSystem", nameof(setLastModifiedTime), "(Ljava/io/File;)Z"));
+                __jniPtr__setReadOnly ??= Marshal.GetDelegateForFunctionPointer<__jniDelegate__setReadOnly>(JNIFrame.GetFuncPtr(__callerID, "java/io/WinNTFileSystem", nameof(setReadOnly), "(Ljava/io/File;)Z"));
                 var jniFrm = new JNIFrame();
                 var jniEnv = jniFrm.Enter(__callerID);
                 try
