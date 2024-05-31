04f40159efe84f9d14e883c0da43b7fc7c1ff4b1
Jerome Haltom <jhaltom@alethic.solutions>
2024-05-31 11:01:24 -0500

    Check specifically for contents.

--- src/IKVM.Tests/Java/java/security/SecurityTests.cs
+++ src/IKVM.Tests/Java/java/security/SecurityTests.cs
@@ -15,10 +15,18 @@ namespace IKVM.Tests.Java.java.security
         public void CanGetProviders()
         {
             var l = global::java.security.Security.getProviders();
+            l.Should().Contain(i => i.toString() == "SUN version 1.8");
+            l.Should().Contain(i => i.toString() == "SunRsaSign version 1.8");
+            l.Should().Contain(i => i.toString() == "SunEC version 1.8");
+            l.Should().Contain(i => i.toString() == "SunJSSE version 1.8");
+            l.Should().Contain(i => i.toString() == "SunJCE version 1.8");
+            l.Should().Contain(i => i.toString() == "SunJGSS version 1.8");
+            l.Should().Contain(i => i.toString() == "SunSASL version 1.8");
+            l.Should().Contain(i => i.toString() == "XMLDSig version 1.8");
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                l.Should().HaveCount(9);
-            else
-                l.Should().HaveCount(8);
+                l.Should().Contain(i => i.toString() == "SunMSCAPI version 1.8");
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                l.Should().Contain(i => i.toString() == "Apple version 1.8");
         }
 
     }
