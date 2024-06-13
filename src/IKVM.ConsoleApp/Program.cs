2bd88d4edeef8633220bda86f5945f1992881199
Jerome Haltom <jhaltom@alethic.solutions>
2024-06-13 11:51:18 -0500

    Merge develop.

--- src/IKVM.ConsoleApp/Program.cs
+++ src/IKVM.ConsoleApp/Program.cs
@@ -1,14 +1,14 @@
-﻿using java.text;
+﻿using java.util;
 
 namespace IKVM.ConsoleApp
 {
 
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            java.text.Normalizer.normalize("hi", Normalizer.Form.NFC);
-            System.Console.ReadLine();
+            ResourceBundle rb = ResourceBundle.getBundle("com.sun.tools.javac.resources.compiler", Locale.US);
         }
 
     }
