05bdf3e4996b17d53e654bf08dfd6343232a5a5e
Jerome Haltom <jhaltom@alethic.solutions>
2024-06-17 15:16:43 -0500

    Simple retrieval of ciphers.

---
+++ src/IKVM.Tests/Java/javax/net/ssl/SSLContextTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+using javax.net.ssl;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Tests.Java.javax.net.ssl
+{
+
+    [TestClass]
+    public class SSLContextTests
+    {
+
+        [TestMethod]
+        public void CanGetDefaultCipherSuites()
+        {
+            var ctx = SSLContext.getDefault();
+            var ptm = ctx.getDefaultSSLParameters();
+            var lst = ptm.getCipherSuites();
+            lst.Should().NotBeNullOrEmpty();
+        }
+
+    }
+
+}
