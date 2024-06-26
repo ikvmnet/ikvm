4537bd55ee8f3e5f035bec499d8bf6ecd15d4b38
Jerome Haltom <jhaltom@kobe.local>
2024-06-25 21:11:13 -0500

    Ensure function is always inline. Set target version on compile as well

--- src/libawt_lwawt/include/osx-arm64/JavaRuntimeSupport2.h
+++ src/libawt_lwawt/include/osx-arm64/JavaRuntimeSupport2.h
@@ -1,7 +1,7 @@
 #include <stdlib.h> 
 #include <string.h>
 
-static inline char* JRSCopyOSVersion2() {
+__attribute__((always_inline)) static inline char* JRSCopyOSVersion2() {
     char *returnString = malloc(16);
     strcpy(returnString, "10.15");
     return returnString;
