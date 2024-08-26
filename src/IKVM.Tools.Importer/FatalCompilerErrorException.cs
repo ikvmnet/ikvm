96194128ab334a3232e9ff043e4d66c08a369913
Jerome Haltom <jhaltom@alethic.solutions>
2024-08-26 09:53:04 -0500

    Use IKVM not IKVMC.

--- src/IKVM.Tools.Importer/FatalCompilerErrorException.cs
+++ src/IKVM.Tools.Importer/FatalCompilerErrorException.cs
@@ -14,9 +14,9 @@ namespace IKVM.Tools.Importer
         /// <param name="evt"></param>
         internal FatalCompilerErrorException(in DiagnosticEvent evt) :
 #if NET8_0_OR_GREATER
-            base($"fatal error IKVMC{evt.Diagnostic.Id}: {string.Format(null, evt.Diagnostic.Message, evt.Args)}")
+            base($"fatal error IKVM{evt.Diagnostic.Id}: {string.Format(null, evt.Diagnostic.Message, evt.Args)}")
 #else
-            base($"fatal error IKVMC{evt.Diagnostic.Id}: {string.Format(evt.Diagnostic.Message, evt.Args.ToArray())}")
+            base($"fatal error IKVM{evt.Diagnostic.Id}: {string.Format(evt.Diagnostic.Message, evt.Args.ToArray())}")
 #endif
         {
 
