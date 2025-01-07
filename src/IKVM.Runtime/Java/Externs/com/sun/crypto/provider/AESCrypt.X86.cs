b9c9e9e0a34cb13f540101876b1321cacb561d81
AliveDevil <AliveDevil@users.noreply.github.com>
2025-01-07 11:19:52 +0100

    Aes Decrypt

--- src/IKVM.Runtime/Java/Externs/com/sun/crypto/provider/AESCrypt.X86.cs
+++ src/IKVM.Runtime/Java/Externs/com/sun/crypto/provider/AESCrypt.X86.cs
@@ -88,5 +88,67 @@ partial class AESCrypt
         xmm_result = Aes.EncryptLast(xmm_result, xmm_temp2);
         _ = xmm_result.TryCopyTo(@out.AsSpan(outOffset));
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void DecryptBlock_X86(byte[] @in, int inOffset, byte[] @out, int outOffset, int[] K)
+    {
+        // ext\openjdk\hotspot\src\cpu\x86\vm\stubGenerator_x86_32.cpp:3125-3210
+
+        Vector128<byte> xmm_temp1, xmm_temp2, xmm_temp3, xmm_temp4;
+#if NET8_0_OR_GREATER
+        var xmm_key_shuf_mask = Vector128.Create(KeyShuffleMask).AsByte();
+        var xmm_result = Vector128.Create(@in, inOffset);
+#else
+        var xmm_key_shuf_mask = Vector128Polyfill.Create(KeyShuffleMask).AsByte();
+        var xmm_result = Vector128Polyfill.Create(@in, inOffset);
+#endif
+
+        xmm_temp1 = LoadKey_X86(K, 0x10, xmm_key_shuf_mask);
+        xmm_temp2 = LoadKey_X86(K, 0x20, xmm_key_shuf_mask);
+        xmm_temp3 = LoadKey_X86(K, 0x30, xmm_key_shuf_mask);
+        xmm_temp4 = LoadKey_X86(K, 0x40, xmm_key_shuf_mask);
+
+        xmm_result = Sse2.Xor(xmm_result, xmm_temp1);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp2);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp3);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp4);
+
+        xmm_temp1 = LoadKey_X86(K, 0x50, xmm_key_shuf_mask);
+        xmm_temp2 = LoadKey_X86(K, 0x60, xmm_key_shuf_mask);
+        xmm_temp3 = LoadKey_X86(K, 0x70, xmm_key_shuf_mask);
+        xmm_temp4 = LoadKey_X86(K, 0x80, xmm_key_shuf_mask);
+
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp1);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp2);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp3);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp4);
+
+        xmm_temp1 = LoadKey_X86(K, 0x90, xmm_key_shuf_mask);
+        xmm_temp2 = LoadKey_X86(K, 0xa0, xmm_key_shuf_mask);
+        xmm_temp3 = LoadKey_X86(K, 0x00, xmm_key_shuf_mask);
+
+        if (K.Length != 11)
+        {
+            xmm_result = Aes.Decrypt(xmm_result, xmm_temp1);
+            xmm_result = Aes.Decrypt(xmm_result, xmm_temp2);
+
+            xmm_temp1 = LoadKey_X86(K, 0xb0, xmm_key_shuf_mask);
+            xmm_temp2 = LoadKey_X86(K, 0xc0, xmm_key_shuf_mask);
+
+            if (K.Length != 13)
+            {
+                xmm_result = Aes.Decrypt(xmm_result, xmm_temp1);
+                xmm_result = Aes.Decrypt(xmm_result, xmm_temp2);
+
+                xmm_temp1 = LoadKey_X86(K, 0xd0, xmm_key_shuf_mask);
+                xmm_temp2 = LoadKey_X86(K, 0xe0, xmm_key_shuf_mask);
+            }
+        }
+
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp1);
+        xmm_result = Aes.Decrypt(xmm_result, xmm_temp2);
+        xmm_result = Aes.DecryptLast(xmm_result, xmm_temp3);
+        _ = xmm_result.TryCopyTo(@out.AsSpan(outOffset));
+    }
 }
 #endif
--- src/IKVM.Runtime/Java/Externs/com/sun/crypto/provider/AESCrypt.cs
+++ src/IKVM.Runtime/Java/Externs/com/sun/crypto/provider/AESCrypt.cs
@@ -17,6 +17,19 @@ internal static partial class AESCrypt
         return false;
     }
 
+    public static bool DecryptBlock(byte[] @in, int inOffset, byte[] @out, int outOffset, int[] K)
+    {
+#if NETCOREAPP3_0_OR_GREATER
+        if (IsSupportedX86)
+        {
+            DecryptBlock_X86(@in, inOffset, @out, outOffset, K);
+            return true;
+        }
+#endif
+
+        return false;
+    }
+
 #if NETCOREAPP3_0_OR_GREATER
     private static ReadOnlySpan<int> KeyShuffleMask => [0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f];
 #endif
