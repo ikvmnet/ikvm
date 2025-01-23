8862ccf2e4cf2ba2e95f00988294152c2c81ec56
AliveDevil <AliveDevil@users.noreply.github.com>
2025-01-23 16:08:29 +0100

    Add AESCrypt_Arm EncryptBlock

--- src/IKVM.Runtime/Util/Com/Sun/Crypto/Provider/AESCrypt_Arm.cs
+++ src/IKVM.Runtime/Util/Com/Sun/Crypto/Provider/AESCrypt_Arm.cs
@@ -29,6 +29,7 @@ namespace IKVM.Runtime.Util.Com.Sun.Crypto.Provider
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void DecryptBlock(ReadOnlySpan<byte> from, Span<byte> to, ReadOnlySpan<int> key)
         {
+            //ext\openjdk\hotspot\src\cpu\aarch64\vm\stubGenerator_aarch64.cpp:2606-2699
             var keylen = key.Length;
 
             var v0 = Vector128Util.LoadUnsafe(in MemoryMarshal.GetReference(from));
@@ -114,6 +115,106 @@ namespace IKVM.Runtime.Util.Com.Sun.Crypto.Provider
             v0 = Aes.Decrypt(v0, v2.AsByte());
 
             v0 = AdvSimd.Xor(v0, v5.AsByte());
+
+            v0.CopyTo(to);
+        }
+
+        /// <summary>
+        /// Implementation of com.sun.crypto.provider.AESCrypt.encryptBlock for the Arm platform.
+        /// Derived from the OpenJDK C code 'stubGenerator_aarch64.cpp:generate_aescrypt_encryptBlock'.
+        /// Keep the structure of the body of this method as close to the orignal C code as possible to facilitate porting changes.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="key"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EncryptBlock(ReadOnlySpan<byte> from, Span<byte> to, ReadOnlySpan<int> key)
+        {
+            //ext\openjdk\hotspot\src\cpu\aarch64\vm\stubGenerator_aarch64.cpp:2505-2597
+            var keylen = key.Length;
+
+            var v0 = Vector128Util.LoadUnsafe(in MemoryMarshal.GetReference(from));
+
+            // AdvSimd.Arm64.Load4xVector128 was added with .NET 9
+            // Manually do the steps required.
+            var v1 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            var v2 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            var v3 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            var v4 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v2 = AdvSimd.ReverseElement8(v2);
+            v3 = AdvSimd.ReverseElement8(v3);
+            v4 = AdvSimd.ReverseElement8(v4);
+            v0 = Aes.Encrypt(v0, v1.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v2.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v3.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v4.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+
+            v1 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v2 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v3 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v4 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v2 = AdvSimd.ReverseElement8(v2);
+            v3 = AdvSimd.ReverseElement8(v3);
+            v4 = AdvSimd.ReverseElement8(v4);
+            v0 = Aes.Encrypt(v0, v1.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v2.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v3.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v4.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+
+            v1 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v2 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v2 = AdvSimd.ReverseElement8(v2);
+
+            if (keylen == 44)
+            {
+                goto doLast;
+            }
+
+            v0 = Aes.Encrypt(v0, v1.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v2.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+
+            v1 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v2 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v2 = AdvSimd.ReverseElement8(v2);
+
+            if (keylen == 52)
+            {
+                goto doLast;
+            }
+
+            v0 = Aes.Encrypt(v0, v1.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v2.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+
+            v1 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v2 = Vector128Util.LoadUnsafe(in RefUtils.Post(ref key, 16));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v2 = AdvSimd.ReverseElement8(v2);
+
+        doLast:;
+
+            v0 = Aes.Encrypt(v0, v1.AsByte());
+            v0 = Aes.InverseMixColumns(v0);
+            v0 = Aes.Encrypt(v0, v2.AsByte());
+
+            v1 = Vector128Util.LoadUnsafe(ref MemoryMarshal.GetReference(key));
+            v1 = AdvSimd.ReverseElement8(v1);
+            v0 = AdvSimd.Xor(v0, v1.AsByte());
 
             v0.CopyTo(to);
         }
