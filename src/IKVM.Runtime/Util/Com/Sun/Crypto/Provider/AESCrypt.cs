4c3f146963615ee4b53f2661825916a6e14651cc
AliveDevil <AliveDevil@users.noreply.github.com>
2025-01-23 17:20:10 +0100

    Add AESCrypt_Arm.EncryptBlock intercept

--- src/IKVM.Runtime/Util/Com/Sun/Crypto/Provider/AESCrypt.cs
+++ src/IKVM.Runtime/Util/Com/Sun/Crypto/Provider/AESCrypt.cs
@@ -74,6 +74,14 @@ namespace IKVM.Runtime.Util.Com.Sun.Crypto.Provider
                 AESCrypt_x86.EncryptBlock(@in.AsSpan(inOffset), @out.AsSpan(outOffset), K);
                 return true;
             }
+
+#if NET6_0_OR_GREATER
+            if (AESCrypt_Arm.IsSupported)
+            {
+                AESCrypt_Arm.EncryptBlock(@in.AsSpan(inOffset), @out.AsSpan(outOffset), K);
+                return true;
+            }
+#endif
 #endif
 
             return false;
